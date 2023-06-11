21e8554485d443e16ce1c43b4e33172b93293bf8
Marc Gueury <marc.gueury@oracle.com>
2023-06-11 22:00:34 +0200

    v1.6

--- basis/bin/deploy_bastion.sh
+++ basis/bin/deploy_bastion.sh
@@ -3,6 +3,16 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/..
 . env.sh -silent
 
+# Run something directly on the DB_NODE (ex RAC)
+if [ -f src/db/db_node_init.sh ]; then
+  # Start ssh-agent to do a Jump from the bastion to the DB_NODE_IP
+  eval "$(ssh-agent -s)"
+  ssh-add $TF_VAR_ssh_private_path
+  scp -o StrictHostKeyChecking=no -oProxyCommand="ssh -W %h:%p opc@$BASTION_IP" target/db_node_init.sh opc@$DB_NODE_IP:/tmp/.
+  ssh -o StrictHostKeyChecking=no -J opc@$BASTION_IP opc@$DB_NODE_IP "chmod +x /tmp/db_node_init.sh; sudo -i -u oracle PREFIX=$PREFIX /tmp/db_node_init.sh 2>&1 | tee -a /tmp/db_node_init.log"
+fi
+
+# In most case, create the DB table from the bastion
 # Using RSYNC allow to reapply the same command several times easily. 
 if command -v rsync &> /dev/null
 then
@@ -12,11 +22,3 @@ else
 fi
 ssh -o StrictHostKeyChecking=no -i $TF_VAR_ssh_private_path opc@$BASTION_IP "export DB_USER=\"$TF_VAR_db_user\";export DB_PASSWORD=\"$TF_VAR_db_password\";export DB_URL=\"$DB_URL\"; bash db/db_init.sh 2>&1 | tee -a db/db_init.log"
 
-# Run something directly on the DB_NODE ? (ex RAC)
-if [ -f src/db/db_node_init.sh ]; then
-  # Start ssh-agent to do a Jump from the bastion to the DB_NODE_IP
-  eval "$(ssh-agent -s)"
-  ssh-add $TF_VAR_ssh_private_path
-  scp -o StrictHostKeyChecking=no -oProxyCommand="ssh -W %h:%p opc@$BASTION_IP" src/db/db_node_init.sh opc@$DB_NODE_IP:/tmp/.
-  ssh -o StrictHostKeyChecking=no -J opc@$BASTION_IP opc@$DB_NODE_IP "export PREFIX=\"$PREFIX\"; chmod +x /tmp/db_node_init.sh; sudo su - oracle /tmp/db_node_init.sh 2>&1 | tee -a /tmp/db_node_init.log"
-fi
--- option/src/db/rac/db_node_init.sh
+++ option/src/db/rac/db_node_init.sh
@@ -1,6 +1,27 @@
+export PREFIX=##PREFIX##
 export INSTANCE1=${PREFIX}1
 export INSTANCE2=${PREFIX}2
 export PDB=PDB1
+
+echo PREFIX=$PREFIX
+echo INSTANCE1=$INSTANCE1
+echo INSTANCE2=$INSTANCE2
+echo ORACLE_UNQNAME=$ORACLE_UNQNAME
+
+# Wait that instance1 is up
+while true ; do
+  echo "Waiting..."
+  srvctl status instance -instance ${INSTANCE1} -db ${ORACLE_UNQNAME} > /tmp/instance_status.txt
+  result=$(grep -nE 'is running' /tmp/instance_status.txt) # -n shows line number
+  echo "DEBUG: Result found is $result"
+  if [ ! -z "$result" ] ; then
+    echo "COMPLETE!"
+    break;
+  fi
+  sleep 5
+  date
+done
+
 
 srvctl add service -db ${ORACLE_UNQNAME} -service jbasic -pdb $PDB -preferred ${INSTANCE1} -available ${INSTANCE2} -failover_restore AUTO -commit_outcome TRUE -failovertype AUTO -replay_init_time 600 -retention 86400 -notification TRUE -drain_timeout 300 -stopoption IMMEDIATE -role PRIMARY
 srvctl start service -db ${ORACLE_UNQNAME} -service jbasic
