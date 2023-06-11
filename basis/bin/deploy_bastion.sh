417e96df11564571167fde4e84dfa9691c31fb4a
Marc Gueury <marc.gueury@oracle.com>
2023-06-12 00:57:28 +0200

    v1.6

--- basis/bin/deploy_bastion.sh
+++ basis/bin/deploy_bastion.sh
@@ -8,7 +8,7 @@ if [ -f src/db/db_node_init.sh ]; then
   # Start ssh-agent to do a Jump from the BASTION to the DB_NODE
   eval "$(ssh-agent -s)"
   ssh-add $TF_VAR_ssh_private_path
-  scp -o StrictHostKeyChecking=no -oProxyCommand="ssh -W %h:%p opc@$BASTION_IP" target/db_node_init.sh opc@$DB_NODE_IP:/tmp/.
+  scp -o StrictHostKeyChecking=no -oProxyCommand="ssh -o StrictHostKeyChecking=no -W %h:%p opc@$BASTION_IP" target/db_node_init.sh opc@$DB_NODE_IP:/tmp/.
   ssh -o StrictHostKeyChecking=no -J opc@$BASTION_IP opc@$DB_NODE_IP "chmod +x /tmp/db_node_init.sh; sudo -i -u oracle PREFIX=$PREFIX /tmp/db_node_init.sh 2>&1 | tee -a /tmp/db_node_init.log"
 fi
 
