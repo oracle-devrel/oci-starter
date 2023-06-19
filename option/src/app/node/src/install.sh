90a32d106df2ae32bf3d22b88f056bdcdfe07c84
Marc Gueury <marc.gueury@oracle.com>
2023-06-19 13:31:25 +0200

    v1.6

--- option/src/app/node/src/install.sh
+++ option/src/app/node/src/install.sh
@@ -4,7 +4,7 @@ cd $SCRIPT_DIR
 
 # Install last version of NodeJS
 # https://yum.oracle.com/oracle-linux-nodejs.html#InstallingNodeOnOL8
-sudo dnf module enable nodejs:18
+sudo dnf module enable -y nodejs:18
 sudo dnf module install -y nodejs
 
 # ORACLE Instant Client (Not needed since Thin driver in oracledb-6.0.0)
