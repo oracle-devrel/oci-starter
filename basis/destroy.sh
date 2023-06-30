e37e5cd7bd69e8dc520307b3ad56f5e168880896
Marc Gueury <marc.gueury@oracle.com>
2023-06-30 14:09:51 +0200

    v1.6

--- basis/destroy.sh
+++ basis/destroy.sh
@@ -3,4 +3,4 @@ export ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && p
 export TARGET_DIR=$ROOT_DIR/target
 
 cd $ROOT_DIR
-. bin/destroy_all.sh $@ 2>&1 | tee $TARGET_DIR/destroy.log
+bin/destroy_all.sh $@ 2>&1 | tee $TARGET_DIR/destroy.log
