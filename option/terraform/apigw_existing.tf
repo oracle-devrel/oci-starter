95837693782baa466fab7ab3ff9506b037badcfa
Marc Gueury <marc.gueury@oracle.com>
2023-08-29 12:57:50 +0200

    v1.6

--- option/terraform/apigw_existing.tf
+++ option/terraform/apigw_existing.tf
@@ -1,14 +1,11 @@
-variable apigw_ocid { default="" }
+variable apigw_ocid {}
+
+data "oci_apigateway_gateway" "starter_apigw" {
+    #Required
+    gateway_id = var.apigw_ocid
+}
 
 locals {
   apigw_ocid = var.apigw_ocid
 }
-
-
-data "oci_apigateway_gateway" "starter_apigw" {
-    #Required
-    count = var.apigw_ocid=="" ? 0 : 1
-    gateway_id = var.apigw_ocid
-}
-
 
