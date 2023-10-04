6fa40deec4057d3c717ab67dc3c5f17f7a57fef3
Marc Gueury <marc.gueury@oracle.com>
2023-10-04 20:26:49 +0200

    v1.6

--- option/src/db/db_free/deploy_db_node.sh
+++ option/src/db/db_free/deploy_db_node.sh
@@ -13,10 +13,10 @@ eval "$(ssh-agent -s)"
 ssh-add $TF_VAR_ssh_private_path
 
 # Wait that the host is up
-until ssh -o StrictHostKeyChecking=no -J opc@$BASTION_IP opc@$DB_NODE_IP echo test; do
-  sleep 5
-  echo "SSH - Waiting for $DB_NODE_IP"
-done
+# until ssh -o StrictHostKeyChecking=no -J opc@$BASTION_IP opc@$DB_NODE_IP echo test; do
+#  sleep 5
+#   echo "SSH - Waiting for $DB_NODE_IP"
+# done
 
 scp -o StrictHostKeyChecking=no -oProxyCommand="ssh -o StrictHostKeyChecking=no -W %h:%p opc@$BASTION_IP" src/db/db_node_init.sh opc@$DB_NODE_IP:/tmp/.
 ssh -o StrictHostKeyChecking=no -J opc@$BASTION_IP opc@$DB_NODE_IP "chmod +x /tmp/db_node_init.sh; sudo -i -u root DB_PASSWORD=$TF_VAR_db_password /tmp/db_node_init.sh 2>&1 | tee -a /tmp/db_node_init.log"
