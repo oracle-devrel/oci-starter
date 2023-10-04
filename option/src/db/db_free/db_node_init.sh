b76de6ce24afa11086162218702c49aed50a4134
Marc Gueury <marc.gueury@oracle.com>
2023-10-04 21:29:50 +0200

    v1.6

--- option/src/db/db_free/db_node_init.sh
+++ option/src/db/db_free/db_node_init.sh
@@ -6,7 +6,7 @@ sudo dnf install -y oracle-database-preinstall-23c
 wget https://download.oracle.com/otn-pub/otn_software/db-free/oracle-database-free-23c-1.0-1.el8.x86_64.rpm
 dnf -y localinstall oracle-database-free-23c-1.0-1.el8.x86_64.rpm
 
-echo DB_PASSWORD=$DB_PASSWORD
+# echo DB_PASSWORD=$DB_PASSWORD
 (echo "${DB_PASSWORD}"; echo "${DB_PASSWORD}";) | /etc/init.d/oracle-free-23c configure
 
 firewall-cmd --zone=public --add-port=1521/tcp --permanent
