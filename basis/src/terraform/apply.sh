bab370472b9fcb0443971c28562cff3068a707ca
Marc Gueury <marc.gueury@oracle.com>
2023-10-07 23:38:18 +0200

    v1.6

--- basis/src/terraform/apply.sh
+++ basis/src/terraform/apply.sh
@@ -3,4 +3,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 . ../../env.sh -silent
 . $BIN_DIR/shared_infra_as_code.sh
-infra_as_code_apply
+infra_as_code_apply $@
