8a74b8e1e93a7577e1a022b111765087b6a476d5
Marc Gueury <marc.gueury@oracle.com>
2023-10-09 08:52:29 +0200

    v1.6

--- basis/bin/shared_infra_as_code.sh
+++ basis/bin/shared_infra_as_code.sh
@@ -70,6 +70,9 @@ resource_manager_create_or_update() {
   # This is a complex way to get them. But it works for multi line variables like TF_VAR_private_key
   excluded=$(env | sed -n 's/^\([A-Z_a-z][0-9A-Z_a-z]*\)=.*/\1/p' | grep -v 'TF_VAR_')
   sh -c 'unset $1; export -p' sh "$excluded" > $TARGET_DIR/tf_var.sh
+  # Nasty WA trick for OCI Devops (not a proper fix)
+  sed -i "s/export maven.home//" $TARGET_DIR/tf_var.sh
+
   echo -n "{" > $VAR_FILE_PATH
   cat $TARGET_DIR/tf_var.sh | sed "s/export TF_VAR_/\"/g" | sed "s/=\"/\": \"/g" | sed ':a;N;$!ba;s/\"\n/\", /g' | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/$/}/'>> $VAR_FILE_PATH
 
