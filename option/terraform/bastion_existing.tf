c0696321c138bdc7c6bd5bc02c8e774bafa61298
Marc Gueury <marc.gueury@oracle.com>
2023-11-24 23:20:48 +0100

    v1.6

--- option/terraform/bastion_existing.tf
+++ option/terraform/bastion_existing.tf
@@ -4,11 +4,6 @@ data "oci_core_instance" "starter_bastion" {
   instance_id = var.bastion_ocid
 }
 
-# Output the private and public IPs of the instance
-output "bastion_private_ips" {
-  value = [data.oci_core_instance.starter_bastion.*.private_ip]
-}
-
-output "bastion_public_ips" {
-  value = [data.oci_core_instance.starter_bastion.*.public_ip]
+output "bastion_public_ip" {
+  value = data.oci_core_instance.starter_bastion.*.public_ip
 }
--- option/terraform/bastion_shared_compute.tf
+++ option/terraform/bastion_shared_compute.tf
@@ -1,8 +1,3 @@
-# Output the private and public IPs of the instance
-output "bastion_private_ip" {
-  value = oci_core_instance.starter_instance.private_ip
-}
-
 output "bastion_public_ip" {
   value = oci_core_instance.starter_instance.public_ip
 }
