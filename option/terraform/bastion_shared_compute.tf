3e9ded51acbd5e08bc141d6d6c849803eaa80665
Marc Gueury <marc.gueury@oracle.com>
2023-11-24 23:18:06 +0100

    v1.6

--- option/terraform/bastion_shared_compute.tf
+++ option/terraform/bastion_shared_compute.tf
@@ -1,8 +1,8 @@
 # Output the private and public IPs of the instance
-output "bastion_private_ips" {
-  value = [oci_core_instance.starter_instance.private_ip]
+output "bastion_private_ip" {
+  value = oci_core_instance.starter_instance.private_ip
 }
 
-output "bastion_public_ips" {
-  value = [oci_core_instance.starter_instance.public_ip]
+output "bastion_public_ip" {
+  value = oci_core_instance.starter_instance.public_ip
 }
