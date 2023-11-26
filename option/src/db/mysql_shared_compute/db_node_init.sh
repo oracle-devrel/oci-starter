a123f41745dec4e5c6582d323a12ac23622343a3
Marc Gueury <marc.gueury@oracle.com>
2023-11-26 15:20:34 +0100

    v1.6

--- option/src/db/mysql_shared_compute/db_node_init.sh
+++ option/src/db/mysql_shared_compute/db_node_init.sh
@@ -3,16 +3,17 @@ wget https://repo.mysql.com//mysql80-community-release-el8-5.noarch.rpm
 yum -y install mysql80-community-release-el8-5.noarch.rpm
 yum repolist enabled | grep "mysql.*-community.*"
 yum -y module disable mysql
-dnf -y install mysql-community-server
-# groupadd -g 27 -o -r mysql
-# useradd -r -g mysql -s /bin/false mysql
+dnf -y install mysql-community-server mysql-shell
 id mysql
 systemctl start mysqld 
 
-# Open the Firewall
-firewall-cmd --zone=public --add-port=3306/tcp --permanent
-firewall-cmd --reload
+export TMP_PASSWORD=`grep 'temporary password' /var/log/mysqld.log | sed 's/.*: //g'` 
+mysqlsh $DB_USER@$DB_URL --password=$TMP_PASSWORD --sql << EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
+EOF
 
-# cat >> /home/opc/.bash_profile << EOF
-# EOF
+# Open the Firewall
+# firewall-cmd --zone=public --add-port=3306/tcp --permanent
+# firewall-cmd --reload
+
 
--- option/terraform/mysql_shared_compute.tf
+++ option/terraform/mysql_shared_compute.tf
@@ -1,9 +1,8 @@
 locals {
     db_host = oci_core_instance.starter_instance.private_ip
     db_port = "3306"
-    db_url = format("%s:%s", local.db_host, local.db_port)
-    // jdbc:mysql://10.1.1.237/db1?user=root&password=xxxxx
-    jdbc_url = format("jdbc:mysql://%s/db1", local.db_host)
+    db_url = format("localhost:%s", local.db_port)
+    jdbc_url = format("jdbc:localhost://%s/db1", local.db_host)
 }
 
 output "mysql_compute_ip" {
