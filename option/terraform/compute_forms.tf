17f663c84ed926fda5ee46adbb2e4ba27b937e07
Marc Gueury <marc.gueury@oracle.com>
2023-12-01 17:05:48 +0100

    v1.6

--- option/terraform/compute_forms.tf
+++ option/terraform/compute_forms.tf
@@ -12,26 +12,40 @@ data "oci_marketplace_listing" "forms_listing" {
 
 # DATA 3 - Get the list of versions for the specific entry (11.3, 12.2.1, ....)
 data "oci_marketplace_listing_packages" "forms_listing_packages" {
+  #Required
   listing_id = data.oci_marketplace_listing.forms_listing.id
+
+  #Optional
   compartment_id = local.lz_appdev_cmp_ocid
   package_version = data.oci_marketplace_listing.forms_listing.default_package_version
 }
 
 # DATA 4 - Get details about a specfic version
 data "oci_marketplace_listing_package" "forms_listing_package" {
+  #Required
   listing_id      = data.oci_marketplace_listing.forms_listing.id
   package_version = data.oci_marketplace_listing_packages.forms_listing_packages.package_version
+
+  #Optional
   compartment_id = local.lz_appdev_cmp_ocid
 }
 
-# DATA 5 - Agreements
+# DATA 5 - agreement for a specific version
 data "oci_marketplace_listing_package_agreements" "forms_listing_package_agreements" {
+  #Required
   listing_id      = data.oci_marketplace_listing.forms_listing.id
-  compartment_id = local.lz_appdev_cmp_ocid
   package_version = data.oci_marketplace_listing_packages.forms_listing_packages.package_version
+
+  #Optional
+  compartment_id = local.lz_appdev_cmp_ocid
 }
 
-# RESOURCE 1 - Agreement for a specific version
+data "oci_core_app_catalog_listing_resource_version" "forms_catalog_listing" {
+  listing_id = data.oci_marketplace_listing_package.forms_listing_package.app_catalog_listing_id
+  resource_version = data.oci_marketplace_listing_package.forms_listing_package.app_catalog_listing_resource_version
+}
+
+# RESOURCE 1 - agreement for a specific version
 resource "oci_marketplace_listing_package_agreement" "forms_listing_package_agreement" {
   #Required
   agreement_id    = data.oci_marketplace_listing_package_agreements.forms_listing_package_agreements.agreements[0].id
@@ -50,7 +64,19 @@ resource "oci_marketplace_accepted_agreement" "forms_accepted_agreement" {
 }
 
 output "image_id" {
-  value = [data.oci_marketplace_listing_package.forms_listing_package.image_id]
+  value = data.oci_marketplace_listing_package.forms_listing_package.image_id
+}
+
+output "forms_listing_id" {
+   value = data.oci_marketplace_listing.forms_listing.id
+}
+
+output "signature" {
+  value = oci_marketplace_listing_package_agreement.forms_listing_package_agreement.signature
+}
+
+output "listing_resource_id" {
+  value = data.oci_core_app_catalog_listing_resource_version.forms_catalog_listing.listing_resource_id
 }
 
 resource oci_core_instance starter_instance {
@@ -58,13 +84,13 @@ resource oci_core_instance starter_instance {
   availability_domain = data.oci_identity_availability_domain.ad.name
   compartment_id      = local.lz_appdev_cmp_ocid
   display_name        = "${var.prefix}-instance"
-  shape               = var.instance_shape
-
+  shape               = "VM.Standard.E4.Flex"
+  
   shape_config {
-    ocpus         = var.instance_ocpus
-    memory_in_gbs = var.instance_shape_config_memory_in_gbs    
+    ocpus         = "1"
+    memory_in_gbs = "16"
   }
-
+  
   create_vnic_details {
     subnet_id                 = data.oci_core_subnet.starter_public_subnet.id
     display_name              = "Primaryvnic"    
@@ -78,10 +104,10 @@ resource oci_core_instance starter_instance {
 
   source_details {
     boot_volume_size_in_gbs = "75"
-    source_id = data.oci_marketplace_listing_package.forms_listing_package.image_id
+    source_id = data.oci_core_app_catalog_listing_resource_version.forms_catalog_listing.listing_resource_id
     source_type = "image"
   }
-
+  /*
   provisioner "file" {    
     connection {
       agent       = false
@@ -142,6 +168,7 @@ resource oci_core_instance starter_instance {
       "sh /u01/oracle/.frm_config/domainconfig.sh"
     ]
   }
+  */
 }
 
 locals {
