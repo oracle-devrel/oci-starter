186cb85f33abe3d35edec76cf512c82f44f9537b
Marc Gueury <marc.gueury@oracle.com>
2023-12-04 18:45:09 +0100

    v1.6

--- option/src/db/psql/db_init.sh
+++ option/src/db/psql/db_init.sh
@@ -5,5 +5,5 @@ cd $SCRIPT_DIR
 # Install PostgreSQL client
 sudo yum -y install postgresql 
 export PGPASSWORD=$DB_PASSWORD
-psql -h $DB_URL -p 5432 -d db1 -U $DB_USER << psql.sql
+psql -h $DB_URL -d postgres -U $DB_USER << psql.sql
 
--- option/terraform/psql_append.tf
+++ option/terraform/psql_append.tf
@@ -1,7 +1,7 @@
 locals {
     db_host = data.oci_psql_db_system.starter_psql.network_details[0].primary_db_endpoint_private_ip
     db_port = "5432"
-    db_url = format("%s:%s", local.db_host, local.db_port)
-    // jdbc:postgresql://localhost:5432/db1
-    jdbc_url = format("jdbc:postgresql://%s:%s/db1", local.db_host, local.db_port )
+    db_url = local.db_host
+    // jdbc:postgresql://localhost:5432/postgres
+    jdbc_url = format("jdbc:postgresql://%s:%s/postgres", local.db_host, local.db_port )
 }
