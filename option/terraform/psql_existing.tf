1a63e3fbf7ceedd2408356f80b75060656c4c049
Marc Gueury <marc.gueury@oracle.com>
2023-12-04 17:11:26 +0100

    v1.6

--- option/terraform/psql_existing.tf
+++ option/terraform/psql_existing.tf
@@ -1,11 +1,6 @@
 // ------------------------ PostgreSQL -----------------------------
 variable "psql_ocid" {}
 
-data "oci_database_autonomous_database" "starter_atp" {
-  #Required
-  autonomous_database_id = var.atp_ocid
-}
-
 # Compatibility with postgresql_existing.tf 
 data "oci_psql_db_system" "starter_psql" {
   #Required
