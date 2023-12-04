83a33c17c23fe73599b0a690e4358c22b85f8eb0
Marc Gueury <marc.gueury@oracle.com>
2023-12-04 17:40:28 +0100

    v1.6

--- option/terraform/psql_append.tf
+++ option/terraform/psql_append.tf
@@ -1,5 +1,5 @@
 locals {
-    db_host = data.oci_psql_db_system.starter_psql.network_details.primary_db_endpoint_private_ip
+    db_host = data.oci_psql_db_system.starter_psql.network_details[0].primary_db_endpoint_private_ip
     db_port = "5432"
     db_url = format("%s:%s", local.db_host, local.db_port)
     // jdbc:postgresql://localhost:5432/db1
