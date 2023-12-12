ec3635f866431fed7b960c10913ca37963a84d67
Marc Gueury <marc.gueury@oracle.com>
2023-12-12 20:46:46 +0100

    v1.6

--- option/src/app/java_micronaut/micronaut_refresh.sh
+++ option/src/app/java_micronaut/micronaut_refresh.sh
@@ -2,15 +2,11 @@ echo "Refresh the sample Micronaut application"
 echo 
 mkdir orig
 mv * orig
-curl --location --request GET 'https://launch.micronaut.io/create/default/com.example.demo?lang=JAVA&build=MAVEN&test=JUNIT&javaVersion=JDK_17&features=data-jpa&features=jdbc-hikari&features=oracle' --output demo.zip
-
-
-curl --location --request GET 'https://launch.micronaut.io/create/default/com.example.demo?lang=JAVA&build=MAVEN&test=JUNIT&javaVersion=JDK_17&features=oracle&features=netty-server&features=hibernate-jpa&features=validation&features=jackson-databind&features=annotation-api&features=graalvm' --output demo.zip
+curl --location --request GET 'https://launch.micronaut.io/create/default/com.example.demo?lang=JAVA&build=MAVEN&test=JUNIT&javaVersion=JDK_17&features=data-jdbc&features=jdbc-hikari&features=oracle' --output demo.zip
 unzip demo.zip
 mv demo/* .
 mv demo/.* .
 rmdir demo
-
 cp orig/*.sh .
 cp orig/openapi_spec.yaml .
 cp orig/app.yaml .
@@ -19,3 +15,5 @@ cp orig/Docker* .
 cp orig/src/main/resources/application.properties src/main/resources/.
 rm src/test/java/com/example/DemoTest.java
 rm src/test/resources/application-test.properties
+
+# Missing redo the .j2 customisation for DeptRepository.java and pom.xml
