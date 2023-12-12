3541420e3d07c87d24f1eace8942af6e228835fe
Marc Gueury <marc.gueury@oracle.com>
2023-12-12 19:39:47 +0100

    v1.6

--- option/src/app/java_micronaut_none/src/main/java/com/example/DeptController.java
+++ option/src/app/java_micronaut_none/src/main/java/com/example/DeptController.java
@@ -7,6 +7,7 @@ import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
 import java.net.URI;
 import java.util.List;
+import java.util.ArrayList;
 
 import jakarta.inject.Inject;
 import static io.micronaut.http.HttpHeaders.LOCATION;
