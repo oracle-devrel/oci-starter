f5cccaad73c4678032296e61cd0f6eafa01b55b8
Marc Gueury <marc.gueury@oracle.com>
2023-12-19 22:52:39 +0100

    v1.6

--- option/terraform/compute_append.tf
+++ option/terraform/compute_append.tf
@@ -3,10 +3,6 @@ output "compute_ip" {
   value = local.compute_public_ip
 }
 
-locals {
-  dns_ip = local.compute_public_ip
-}
-
 output "ui_url" {
   value = format("http://%s", local.compute_public_ip) 
 }
