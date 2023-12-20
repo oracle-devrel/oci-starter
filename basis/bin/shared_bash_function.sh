3d2006d61a170fb5757a2adc12b8282416cef839
Marc Gueury <marc.gueury@oracle.com>
2023-12-20 18:41:34 +0100

    v1.6

--- basis/bin/shared_bash_function.sh
+++ basis/bin/shared_bash_function.sh
@@ -239,9 +239,9 @@ get_ui_url() {
   elif [ "$TF_VAR_deploy_strategy" == "instance_pool" ]; then
     get_output_from_tfstate UI_URL pool_lb_url 
   elif [ "$TF_VAR_deploy_strategy" == "kubernetes" ]; then
-    export DNS_IP=`kubectl get service -n ingress-nginx ingress-nginx-controller -o jsonpath="{.status.loadBalancer.ingress[0].ip}"`
-    export LB_OCID=`oci lb load-balancer list --compartment-id $TF_VAR_compartment_ocid | jq -r '.data[] | select(.["ip-addresses"][0]["ip-address"]=="'$DNS_IP'") | .id'`  
-    export UI_URL=http://${LB_IP}/${TF_VAR_prefix}
+    # export INGRESS_LB_IP=`kubectl get service -n ingress-nginx ingress-nginx-controller -o jsonpath="{.status.loadBalancer.ingress[0].ip}"`
+    # export INGRESS_LB_OCID=`oci lb load-balancer list --compartment-id $TF_VAR_compartment_ocid | jq -r '.data[] | select(.["ip-addresses"][0]["ip-address"]=="'$INGRESS_LB_IP'") | .id'`  
+    export UI_URL=http://${INGRESS_LB_OCID}/${TF_VAR_prefix}
   elif [ "$TF_VAR_deploy_strategy" == "function" ] || [ "$TF_VAR_deploy_strategy" == "container_instance" ]; then  
     if [ "$TF_VAR_certificate_ocid" == "" ]; then
        export UI_URL=https://${APIGW_HOSTNAME}/${TF_VAR_prefix}
