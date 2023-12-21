48f3dd8320fa66b9e1a700354028a6298d20bcc3
Marc Gueury <marc.gueury@oracle.com>
2023-12-21 20:37:25 +0100

    v1.6

--- py_oci_starter.py
+++ py_oci_starter.py
@@ -707,7 +707,7 @@ def create_dir_shared():
         cp_terraform("bastion_shared_compute.tf")   
     elif 'bastion_ocid' in params:
         cp_terraform("bastion_existing.tf")
-    elif params.get('database')!='none':
+    elif params.get('database')!='none' or 'tls' in params:
         cp_terraform("bastion.tf")
 
 #----------------------------------------------------------------------------
