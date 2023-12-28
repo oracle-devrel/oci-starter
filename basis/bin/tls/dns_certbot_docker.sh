e8e01603b54a50dfce0f5867dfbb0b4947df3947
Marc Gueury <marc.gueury@oracle.com>
2023-12-28 10:03:21 +0100

    v1.6

--- basis/bin/tls/dns_certbot_docker.sh
+++ basis/bin/tls/dns_certbot_docker.sh
@@ -11,7 +11,8 @@ cp $BIN_DIR/tls/dns* $TARGET_DIR/certbot_shared/.
 docker run -it --rm --name certbot \
             -v "$TARGET_DIR/letsencrypt:/etc/letsencrypt" \
             -v "$TARGET_DIR/certbot_shared:/certbot_shared" \
-            certbot/certbot -d $TF_VAR_dns_name --agree-tos --register-unsafely-without-email --manual --preferred-challenges dns --manual-auth-hook /certbot_shared/dns_
-            challenge.sh --manual-cleanup-hook /certbot_shared/dns_challenge_clean.sh --disable-hook-validation --force-renewal certonly
+            certbot/certbot -d $TF_VAR_dns_name --agree-tos --register-unsafely-without-email --manual --preferred-challenges dns \
+            --manual-auth-hook /certbot_shared/dns_challenge.sh --manual-cleanup-hook /certbot_shared/dns_challenge_clean.sh \
+            --disable-hook-validation --force-renewal certonly
 
 
