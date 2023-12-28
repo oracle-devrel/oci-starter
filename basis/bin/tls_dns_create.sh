d501a92526b8edae1494a740064ef8872179ce2c
Marc Gueury <marc.gueury@oracle.com>
2023-12-28 11:13:40 +0100

    v1.6

--- basis/bin/tls_dns_create.sh
+++ basis/bin/tls_dns_create.sh
@@ -12,14 +12,8 @@ if [ "$TF_VAR_dns_name" == "" ]; then
   exit 1
 fi
 
-mkdir -p $TARGET_DIR/letsencrypt
 mkdir -p $TARGET_DIR/certbot_shared
-
 cp $BIN_DIR/tls/dns* $TARGET_DIR/certbot_shared/.
 
-docker run -it --rm --name certbot \
-            -v "$TARGET_DIR/letsencrypt:/etc/letsencrypt" \
-            -v "$TARGET_DIR/certbot_shared:/certbot_shared" \
-            certbot/certbot -d $TF_VAR_dns_name --agree-tos --register-unsafely-without-email --manual --preferred-challenges dns \
-            --manual-auth-hook /certbot_shared/dns_challenge.sh --manual-cleanup-hook /certbot_shared/dns_challenge_clean.sh \
-            --disable-hook-validation --force-renewal certonly
+# docker run -it --rm --name certbot --entrypoint /bin/sh certbot/certbot
+docker run -it --rm --name certbot -v "$TARGET_DIR/certbot_shared:/certbot_shared" --entrypoint /certbot_shared/dns_certbot_entrypoint.sh certbot/certbot $TF_VAR_dns_name
