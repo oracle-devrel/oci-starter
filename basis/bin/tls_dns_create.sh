8509e823af68a37257def26dfbdab107bf81b112
Marc Gueury <marc.gueury@oracle.com>
2023-12-28 13:52:52 +0100

    v1.6

--- basis/bin/tls_dns_create.sh
+++ basis/bin/tls_dns_create.sh
@@ -12,17 +12,22 @@ if [ "$TF_VAR_dns_name" == "" ]; then
   exit 1
 fi
 
+# Delete the directory in case of a previous run
+if [ -d $TARGET_DIR/certbot_shared ]; then
+  rm -Rf $TARGET_DIR/certbot_shared
+fi   
 mkdir -p $TARGET_DIR/certbot_shared
 cp $BIN_DIR/tls/dns* $TARGET_DIR/certbot_shared/.
 
 # docker run -it --rm --name certbot --entrypoint /bin/sh certbot/certbot
 docker run -it --rm --name certbot -v "$TARGET_DIR/certbot_shared:/certbot_shared" --entrypoint /bin/sh certbot/certbot /certbot_shared/dns_certbot_entrypoint.sh $TF_VAR_dns_name
 
+# Copy the certificate directory to src/tls
 if [ -d $TARGET_DIR/certbot_shared/$TF_VAR_dns_name ]; then
   mkdir -p $PROJECT_DIR/src/tls
   cp -R $TARGET_DIR/certbot_shared/$TF_VAR_dns_name $PROJECT_DIR/src/tls/.
   rm -R $TARGET_DIR/certbot_shared/$TF_VAR_dns_name
 else
-  echo "ERROR: certificate not found. Check Errors before"
+  echo "ERROR: certificate not found. Check for errors before."
   exit 1
 fi
