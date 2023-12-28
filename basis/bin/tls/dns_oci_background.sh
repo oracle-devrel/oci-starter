4cf05438bd0bec25bea4053e41c7ab2de58ffed3
Marc Gueury <marc.gueury@oracle.com>
2023-12-28 10:18:49 +0100

    v1.6

--- basis/bin/tls/dns_oci_background.sh
+++ basis/bin/tls/dns_oci_background.sh
@@ -6,6 +6,8 @@ export CERTBOT_VALIDATION=`cat $TARGET_DIR/certbot_shared/CERTBOT_VALIDATION`
 export TF_VAR_dns_acme_challenge=_acme-challenge.${CERBOT_DOMAIN}
 export TF_VAR_dns_data=$CERTBOT_VALIDATION
 oci dns record rrset update --force --zone-name-or-id $TF_VAR_dns_zone_name --domain $TF_VAR_dns_acme_challenge --rtype 'TXT' --items '[{"domain":"'$TF_VAR_dns_acme_challenge'", "rdata":"'$TF_VAR_dns_data'", "rtype":"TXT","ttl":300}]' --wait-for-state ACTIVE --wait-for-state FAILED
+# XXX Check that DNS is really propagated ? 
+echo "done" > $TARGET_DIR/certbot_shared/DNS_CREATED
 
 # Wait that Certbot create the validation token
 wait_file $TARGET_DIR/certbot_shared/CERTBOT_DOMAIN_CLEAN ]
