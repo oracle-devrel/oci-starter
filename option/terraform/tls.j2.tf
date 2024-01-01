48530039c7739abafb39ac0ecd982f8ba5a29e0a
Marc Gueury <marc.gueury@oracle.com>
2024-01-01 23:03:03 +0100

    v1.6

--- option/terraform/tls.j2.tf
+++ option/terraform/tls.j2.tf
@@ -13,12 +13,6 @@ locals {
 }
 
 resource "oci_dns_rrset" "starter_rrset" {
-{%- if deploy == "kubernetes" %}
-    count = var.ingress_ip=="" ? 0 : 1
-{%- else %}  
-    count = var.dns_zone_name=="" ? 0 : 1
-{%- endif %}       
-
     #Required
     zone_name_or_id = var.dns_zone_name
     domain = var.dns_name
@@ -34,9 +28,7 @@ resource "oci_dns_rrset" "starter_rrset" {
 }
 
 {%- if deploy == "instance_pool" %}  
-resource "oci_load_balancer_listener" "starter-lb-https-listener" {
-    #Required
-  count                    = var.certificate_ocid=="" ? 0 : 1
+resource "oci_load_balancer_listener" "starter_lb_https_listener" {
   load_balancer_id         = oci_load_balancer.starter_pool_lb.id
   name                     = "HTTP-443"
   default_backend_set_name = oci_load_balancer_backend_set.starter_pool_backend_set.name
@@ -56,39 +48,6 @@ resource "oci_load_balancer_listener" "starter-lb-https-listener" {
     verify_peer_certificate = false
   }
 }
-
-{%- if tls == "new" %}  
-resource oci_load_balancer_backend_set starter-bastion-bes {
-  health_checker {
-    protocol       = "HTTP"
-    url_path       = "/"
-  }
-  load_balancer_id = oci_load_balancer.starter_pool_lb.id
-  name             = "${var.prefix}-bastion-bes"
-  policy           = "ROUND_ROBIN"
-}
-
-resource oci_load_balancer_backend starter-bastion-be {
-  load_balancer_id = oci_load_balancer.starter_pool_lb.id
-  backendset_name  = oci_load_balancer_backend_set.starter-bastion-bes.name
-  ip_address       = oci_core_instance.starter_bastion.private_ip 
-  port             = "80"
-  weight           = "1"
-}
-
-resource oci_load_balancer_path_route_set starter-bastion-routeset {
-  load_balancer_id = oci_load_balancer.starter_pool_lb.id
-  name             = "${var.prefix}-bastion-routeset"
-  path_routes {
-    backend_set_name = oci_load_balancer_backend_set.starter-bastion-bes.name
-    path             = "/.well-known/acme-challenge"
-    path_match_type {
-      match_type = "PREFIX_MATCH"
-    }
-  }
-}
-{%- endif %}
-
 {%- endif %}
 
 
