9323f07f6dab7534398ea92ba31126013d4c262e
Marc Gueury <marc.gueury@oracle.com>
2024-01-19 21:58:12 +0100

    v2.0

--- option/src/app/php_opensearch/src/html/dept.php
+++ option/src/app/php_opensearch/src/html/dept.php
@@ -2,20 +2,15 @@
 
 // Connect to the database
 $db_url = get_cfg_var( "app.cfg.DB_URL" );
-$response = file_get_contents("https://" .  db_url . ":9200/dept/_search?size=1000&scroll=1m&pretty=true");
+$response = file_get_contents("https://" .  $db_url . ":9200/dept/_search?size=1000&scroll=1m&pretty=true");
 $response = json_decode($response);
 
 // Select all rows from the table
 $result = array();
-foreach ($hit as $response.hits.hits) {
-  $d = array(
-    "deptno" => $hit["deptno"],
-    "dname" => $hit["dname"],
-    "loc" => $hit["loc"]
-  );
-  array_push( $result, $d );
+foreach ($response->hits->hits as $hit) {
+  $result[] = array( "deptno" => $hit->_source->deptno, "dname" => $hit->_source->dname, "loc" => $hit->_source->loc );
 }
 // Encode the array as JSON and output it
 header('Content-type: application/json');
-echo json_encode($results);
+echo json_encode($result);
 ?>
