2ef60301e76d25d0f6d10da7da8f84f92a2727ff
Marc Gueury <marc.gueury@oracle.com>
2024-01-23 18:23:26 +0100

    v2.0

---
+++ option/src/app/fn/fn_python/func.j2.py
@@ -0,0 +1,17 @@
+{% import "python.j2_macro" as m with context %}
+import io
+import json
+import logging
+import os
+import oracledb
+
+from fdk import response
+
+def handler(ctx, data: io.BytesIO = None):
+    {{ m.dept() }}
+    return response.Response(
+        ctx, response_data=json.dumps(a),
+        headers={"Content-Type": "application/json"}
+    )
+
+    
