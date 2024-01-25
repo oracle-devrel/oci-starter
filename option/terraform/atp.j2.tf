21fbbfe2945d19c137bf64b718b2f42d42476080
Marc Gueury <marc.gueury@oracle.com>
2024-01-25 22:30:21 +0100

    v2.0

--- option/terraform/atp.j2.tf
+++ option/terraform/atp.j2.tf
@@ -45,15 +45,6 @@ data "oci_database_autonomous_database" "starter_atp" {
 {%- endif %}
 
 {%- if group_name is not defined %}
-locals {
-    db_host = data.oci_mysql_mysql_db_system.starter_mysql.endpoints.0.ip_address 
-    db_port = data.oci_mysql_mysql_db_system.starter_mysql.endpoints.0.port
-    db_url = format("%s:%s", local.db_host, local.db_port)
-    // jdbc:mysql://10.1.1.237/db1?user=root&password=xxxxx
-    jdbc_url = format("jdbc:mysql://%s/db1", local.db_host)
-}
-{%- endif %}  
-
 // -- Locals ----------------------------------------------------------------
 locals {
   # Create List of 'name' values from source objet list
@@ -92,4 +83,4 @@ resource "oci_identity_policy" "starter-policy" {
   ]
 }
 */
-
+{%- endif %}  
