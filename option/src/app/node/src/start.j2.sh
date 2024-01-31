81f03c3581026802f610aed7ae116a3ca3403cf6
Marc Gueury <marc.gueury@oracle.com>
2024-01-31 14:47:06 +0100

    v2.0

--- option/src/app/node/src/start.j2.sh
+++ option/src/app/node/src/start.j2.sh
@@ -2,14 +2,13 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-{%- if db_family == "nosql" -%}
+{% if db_family == "nosql" -%}
 export TF_VAR_compartment_ocid=`curl -s -H "Authorization: Bearer Oracle" -L http://169.254.169.254/opc/v2/instance/ | jq -r .compartmentId`
 export TF_VAR_region=`curl -s -H "Authorization: Bearer Oracle" -L http://169.254.169.254/opc/v2/instance/ | jq -r .region`
 {%- else -%}
 export DB_USER="##DB_USER##"
 export DB_PASSWORD="##DB_PASSWORD##"
 export DB_URL="##DB_URL##"
-{%- endif -%}
-
+{%- endif %}
 node rest.js > app.log 2>&1 
 
