3a8f03dc1aa7c35cd613bd0d4092bf059cfe5443
Marc Gueury <marc.gueury@oracle.com>
2024-02-11 20:38:13 +0100

    v2.0

--- option/src/app/java_springboot/src/main/java/com/example/demo/DemoController.j2.java
+++ option/src/app/java_springboot/src/main/java/com/example/demo/DemoController.j2.java
@@ -1,30 +1,27 @@
-{% import "java.j2_macro" as m with context %}
+{%import"java.j2_macro"as m with context%}
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.net.Inet4Address;
-import java.sql.*;
-{{ m.import() }}
+import java.sql.*;{{m.import()}}
 
 @RestController
 
 public class DemoController {
+    public record Dept(int deptno, String dname, String loc) {
+    };
 
-  public record Dept( int deptno, String dname, String loc ) {}; 
+    {{ m.constructor() }}
 
-  @Autowired
-  public DemoController(DbProperties properties) {
-  }
+    @RequestMapping(value = "/dept", method = RequestMethod.GET, produces = { "application/json" })  
+    public List<Dept> query() throws Exception {
+        {{ m.dept() }}
+    }
 
-  @RequestMapping(value = "/dept", method = RequestMethod.GET, produces = { "application/json" })  
-  public List<Dept> query() throws Exception {
-    {{ m.dept() }}
-  }
-
-  @RequestMapping(value = "/info", method = RequestMethod.GET, produces ={ "text/plain" })  
-  public String info() throws Exception {
-    String IP = (System.getenv("POD_IP")==null)?Inet4Address.getLocalHost().getHostAddress():System.getenv("POD_IP");
-    return "Java - SpringBoot - {{ dbName }} - IP=" + IP; 
-  }  
+    @RequestMapping(value = "/info", method = RequestMethod.GET, produces = { "text/plain" })
+    public String info() throws Exception {
+        String IP = (System.getenv("POD_IP") == null) ? Inet4Address.getLocalHost().getHostAddress():System.getenv("POD_IP");
+        return "Java - SpringBoot - {{ dbName }} - IP=" + IP;
+    }
 }
