f57d53660328668f249889ba80e73ac57c81e142
Marc Gueury <marc.gueury@oracle.com>
2024-02-19 15:13:21 +0100

    v2.0

---
+++ option/src/app/java_springboot_nosql/src/main/java/com/example/demo/DemoRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo;
+  
+import com.oracle.nosql.spring.data.repository.NosqlRepository;
+
+interface DemoRepository extends NosqlRepository<Dept, Long> {
+}
---
+++ option/src/app/java_springboot_nosql/src/main/java/com/example/demo/Dept.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "DEPT")
+public class Dept {
+
+    @Id
+    private int deptno;
+    private String dname;
+    private String loc;
+
+    public Dept() {
+    }
+
+    public Dept( int deptno, String dname, String loc) {
+        this.deptno = deptno;
+        this.dname = dname;
+        this.loc = loc;
+    }
+
+    public int getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(int deptno) {
+        this.deptno = deptno;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
+}
