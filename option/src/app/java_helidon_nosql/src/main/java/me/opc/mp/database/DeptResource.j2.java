492e2155f162fe7f28edd9519ee8713dc45f77b7
Marc Gueury <marc.gueury@oracle.com>
2024-03-11 21:09:36 +0100

    v2.0

---
+++ option/src/app/java_helidon_nosql/src/main/java/me/opc/mp/database/DeptResource.j2.java
@@ -0,0 +1,48 @@
+# https://blogs.oracle.com/nosql/post/getting-started-accessing-oracle-nosql-database-using-jakarta-nosql
+# 
+{% import "java.j2_macro" as m with context %}
+package me.opc.mp.database;
+
+import jakarta.persistence.*;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.inject.*;
+import jakarta.annotation.*;
+import jakarta.enterprise.*;
+{{ m.import() }}
+
+/**
+ * Dept Table 
+ */
+@Path("/")
+public class DeptResource {
+    @Inject
+    private DeptRepository repository;
+
+    @PostConstruct 
+    private void init() {
+        try {
+            System.out.println("Insert row " + repository.save(new Dept(10, "ACCOUNTING", "BRUSSELS")));
+            System.out.println("Insert row " + repository.save(new Dept(20, "RESEARCH", "SPRING NOSQL")));
+            System.out.println("Insert row " + repository.save(new Dept(30, "SALES", "ROME")));
+            System.out.println("Insert row " + repository.save(new Dept(40, "OPERATIONS", "MADRID")));
+        } catch (Exception e ) {
+            System.err.println("Init Exception:" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @GET
+    @Path("dept")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Dept> getDept() throws Exception {
+        return repository.findAll().toList();
+    }
+
+    @GET
+    @Path("info")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getInfo() {
+        return "Java - Helidon - {{ dbName }}";
+    }
+}
