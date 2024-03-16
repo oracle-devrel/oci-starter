6836e9a631dce2c197cc6fb824705bdda6054a9a
Marc Gueury <marc.gueury@oracle.com>
2024-03-16 19:56:55 +0100

    v2.0

--- option/terraform/network.j2.tf
+++ option/terraform/network.j2.tf
@@ -253,6 +253,8 @@ resource "oci_core_route_table" "starter_route_private" {
 }
 
 {%- else %} 
+
+# NAT Gateway
 resource "oci_core_nat_gateway" "starter_nat_gateway" {
   compartment_id = local.lz_network_cmp_ocid
   vcn_id         = oci_core_vcn.starter_vcn.id
@@ -260,11 +262,29 @@ resource "oci_core_nat_gateway" "starter_nat_gateway" {
   freeform_tags  = local.freeform_tags
 }
 
+# Service Gateway
+resource "oci_core_service_gateway" "starter_service_gateway" {
+  compartment_id = local.lz_network_cmp_ocid
+  services {
+    service_id = data.oci_core_services.all_services.services[0]["id"]
+  }
+  vcn_id         = oci_core_vcn.starter_vcn.id
+
+  display_name   = "${var.prefix}-service-gateway"
+  freeform_tags  = local.freeform_tags
+}
+
+# Route Private Subnet
 resource "oci_core_route_table" "starter_route_private" {
   compartment_id = local.lz_network_cmp_ocid
   vcn_id         = oci_core_vcn.starter_vcn.id
   display_name   = "${var.prefix}-route-private"
 
+  route_rules {
+    destination       = data.oci_core_services.all_services.services[0]["cidr_block"]
+    destination_type  = "SERVICE_CIDR_BLOCK"
+    network_entity_id = oci_core_service_gateway.starter_service_gateway.id
+  }  
   route_rules {
     destination       = "0.0.0.0/0"
     destination_type  = "CIDR_BLOCK"
