a51907d53a88747cc53e0d03227538b4f4ffa127
Marc Gueury <marc.gueury@oracle.com>
2024-05-04 00:38:52 +0200

    v2.0

--- option/src/db/db_free/db_node_init.sh
+++ option/src/db/db_free/db_node_init.sh
@@ -1,5 +1,5 @@
 sudo dnf install -y podman
-podman run -d -p 1521:1522 -p 1522:1522 -p 8443:8443 -p 27017:27017 -e WORKLOAD_TYPE='ATP' -e WALLET_PASSWORD=LiveLab_123 -e ADMIN_PASSWORD=LiveLab_123 --cap-add SYS_ADMIN --device /dev/fuse --name adb-free --volume adb_container_volume:/u01/data container-registry.oracle.com/database/adb-free:latest-23ai
+podman run -d -p 1521:1522 -p 1522:1522 -p 8443:8443 -p 27017:27017 -e WORKLOAD_TYPE='ATP' -e WALLET_PASSWORD=${DB_PASSWORD} -e ADMIN_PASSWORD=${DB_PASSWORD} --cap-add SYS_ADMIN --device /dev/fuse --name adb-free --volume adb_container_volume:/u01/data container-registry.oracle.com/database/adb-free:latest-23ai
 alias adb-cli="podman exec adb-free adb-cli"
 
 # Open the Firewall
