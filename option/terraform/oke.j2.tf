a5e29cdf8eae019147b727f86f8f62bda052dc2e
Marc Gueury <marc.gueury@oracle.com>
2024-05-12 07:39:40 +0200

    v2.0

--- option/terraform/oke.j2.tf
+++ option/terraform/oke.j2.tf
@@ -7,14 +7,30 @@ locals {
 
 {%- else %}  
 
+#----------------------------------------------------------------------------
+# VARIABLES
+
 variable "oke_shape" {
   {%- if shape == "ampere" %}
-  default = "Pod.Standard.A1.Flex"
+  default = "VM.Standard.A1.Flex"
   {%- else %}
-  default = "Pod.Standard.E4.Flex"
+  default = "VM.Standard.AMD.Generic"
   {%- endif %}
 }
 
+variable "node_pool_size" {
+  default = 1
+}
+
+variable "cluster_options_persistent_volume_config_defined_tags_value" {
+  default = "value"
+}
+
+variable "kubernetes_version" {
+  default = "v1.29.1"
+}
+
+# CIDR
 locals {
   oke_cidr_nodepool     = "10.0.10.0/24"
   oke_cidr_loadbalancer = "10.0.20.0/24"
@@ -23,100 +39,118 @@ locals {
   oke_cidr_services     = "10.2.0.0/16"
 }
 
-resource "oci_core_internet_gateway" "generated_oci_core_internet_gateway" {
-	compartment_id = var.compartment_ocid
-	display_name = "oke-igw-quick-starter-virtual-168cd3edc"
-	enabled = "true"
-	vcn_id = "${data.oci_core_vcn.starter_vcn.id}"
+#----------------------------------------------------------------------------
+# DATA
+
+data "oci_containerengine_cluster_option" "starter_cluster_option" {
+  cluster_option_id = "all"
 }
 
-resource "oci_core_nat_gateway" "generated_oci_core_nat_gateway" {
-	compartment_id = var.compartment_ocid
-	display_name = "oke-ngw-quick-starter-virtual-168cd3edc"
-	vcn_id = "${data.oci_core_vcn.starter_vcn.id}"
+data "oci_containerengine_node_pool_option" "starter_node_pool_option" {
+  node_pool_option_id = "all"
 }
 
-resource "oci_core_service_gateway" "generated_oci_core_service_gateway" {
-	compartment_id = var.compartment_ocid
-	display_name = "oke-sgw-quick-starter-virtual-168cd3edc"
-	services {
-		service_id = "ocid1.service.oc1.eu-frankfurt-1.aaaaaaaa7ncsqkj6lkz36dehifizupyn6qjqsmtepsegs23yyntnsy7qrvea"
-	}
-	vcn_id = "${data.oci_core_vcn.starter_vcn.id}"
+data "oci_identity_availability_domain" "ad1" {
+  compartment_id = var.tenancy_ocid
+  ad_number      = 1
 }
 
-resource "oci_core_route_table" "generated_oci_core_route_table" {
-	compartment_id = var.compartment_ocid
-	display_name = "oke-private-routetable-starter-virtual-168cd3edc"
-	route_rules {
-		description = "traffic to the internet"
-		destination = "0.0.0.0/0"
-		destination_type = "CIDR_BLOCK"
-		network_entity_id = "${oci_core_nat_gateway.generated_oci_core_nat_gateway.id}"
-	}
-	route_rules {
-		description = "traffic to OCI services"
-		destination = "all-fra-services-in-oracle-services-network"
-		destination_type = "SERVICE_CIDR_BLOCK"
-		network_entity_id = "${oci_core_service_gateway.generated_oci_core_service_gateway.id}"
-	}
-	vcn_id = "${data.oci_core_vcn.starter_vcn.id}"
+data "oci_identity_availability_domain" "ad2" {
+  compartment_id = var.tenancy_ocid
+  ad_number      = 2
 }
 
-resource "oci_core_subnet" "service_lb_subnet" {
-	cidr_block = local.oke_cidr_loadbalancer
-	compartment_id = var.compartment_ocid
-	display_name = "oke-svclbsubnet-quick-starter-virtual-168cd3edc-regional"
-	dns_label = "lbsube0f83562f"
-	prohibit_public_ip_on_vnic = "false"
-	route_table_id = "${oci_core_default_route_table.generated_oci_core_default_route_table.id}"
-	security_list_ids = ["${data.oci_core_vcn.starter_vcn.default_security_list_id}"]
-	vcn_id = "${data.oci_core_vcn.starter_vcn.id}"
+data "oci_core_images" "shape_specific_images" {
+  #Required
+  compartment_id = var.tenancy_ocid
+  shape     = var.oke_shape
 }
 
-resource "oci_core_subnet" "node_subnet" {
-	cidr_block = local.oke_cidr_nodepool
-	compartment_id = var.compartment_ocid
-	display_name = "oke-nodesubnet-quick-starter-virtual-168cd3edc-regional"
-	dns_label = "subfe50c27a4"
-	prohibit_public_ip_on_vnic = "true"
-	route_table_id = "${oci_core_route_table.generated_oci_core_route_table.id}"
-	security_list_ids = ["${oci_core_security_list.node_sec_list.id}"]
-	vcn_id = "${data.oci_core_vcn.starter_vcn.id}"
+locals {
+  # all_images = "${data.oci_core_images.shape_specific_images.images}"
+  # all_sources = "${data.oci_containerengine_node_pool_option.starter_node_pool_option.sources}"
+  # compartment_images = [for image in local.all_images : image.id if length(regexall("Oracle-Linux-[0-9]*.[0-9]*-20[0-9]*",image.display_name)) > 0 ]
+  # oracle_linux_images = [for source in local.all_sources : source.image_id if length(regexall("Oracle-Linux-[0-9]*.[0-9]*-20[0-9]*",source.source_name)) > 0]
+  # image_id = tolist(setintersection( toset(local.compartment_images), toset(local.oracle_linux_images)))[0]
+  image_id = data.oci_core_images.oraclelinux.images.0.id
+}
+  
+
+#----------------------------------------------------------------------------
+# SECURITY LISTS
+
+resource "oci_core_security_list" "starter_seclist_lb" {
+  compartment_id = local.lz_network_cmp_ocid
+  vcn_id         = data.oci_core_vcn.starter_vcn.id
+  display_name   = "${var.prefix}-seclist-lb"
+
+  ingress_security_rules {
+    protocol  = "6" // tcp
+    source    = "0.0.0.0/0"
+    stateless = false
+
+    tcp_options {
+      min = 443
+      max = 443
+    }
+  }
+
+  ingress_security_rules {
+    protocol  = "6" // tcp
+    source    = "0.0.0.0/0"
+    stateless = false
+
+    tcp_options {
+      min = 80
+      max = 80
+    }
+  }
+
+  ingress_security_rules {
+    protocol  = "6" // tcp
+    source    = "0.0.0.0/0"
+    stateless = false
+
+    tcp_options {
+      min = 8080
+      max = 8080
+    }
+  }
+
+  egress_security_rules {
+    description      = "Allow TCP traffic from Load Balancers to node ports"
+    destination      = local.oke_cidr_nodepool
+    destination_type = "CIDR_BLOCK"
+    protocol  = "6"
+    stateless = "false"
+    tcp_options {
+      min = 30000
+      max = 32767
+    }
+  }
+  egress_security_rules {
+    description      = "Allow TCP traffic from Load Balancers to node ports"
+    destination      = local.oke_cidr_nodepool
+    destination_type = "CIDR_BLOCK"
+    protocol  = "6"
+    stateless = "false"
+    tcp_options {
+      min = 10256
+      max = 10256
+    }
+  }
+
+  freeform_tags = local.freeform_tags
 }
 
-resource "oci_core_subnet" "kubernetes_api_endpoint_subnet" {
-	cidr_block = local.oke_cidr_api
-	compartment_id = var.compartment_ocid
-	display_name = "oke-k8sApiEndpoint-subnet-quick-starter-virtual-168cd3edc-regional"
-	dns_label = "sub49af870c0"
-	prohibit_public_ip_on_vnic = "false"
-	route_table_id = "${oci_core_default_route_table.generated_oci_core_default_route_table.id}"
-	security_list_ids = ["${oci_core_security_list.kubernetes_api_endpoint_sec_list.id}"]
-	vcn_id = "${data.oci_core_vcn.starter_vcn.id}"
-}
+#----------------------------------------------------------------------------
 
-resource "oci_core_default_route_table" "generated_oci_core_default_route_table" {
-	display_name = "oke-public-routetable-starter-virtual-168cd3edc"
-	route_rules {
-		description = "traffic to/from internet"
-		destination = "0.0.0.0/0"
-		destination_type = "CIDR_BLOCK"
-		network_entity_id = "${oci_core_internet_gateway.generated_oci_core_internet_gateway.id}"
-	}
-	manage_default_resource_id = "${data.oci_core_vcn.starter_vcn.default_route_table_id}"
-}
+resource "oci_core_security_list" "starter_seclist_node" {
+  compartment_id = local.lz_network_cmp_ocid
+  vcn_id         = data.oci_core_vcn.starter_vcn.id
+  display_name   = "${var.prefix}-seclist-node"
 
-resource "oci_core_security_list" "service_lb_sec_list" {
-	compartment_id = var.compartment_ocid
-	display_name = "oke-svclbseclist-quick-starter-virtual-168cd3edc"
-	vcn_id = "${data.oci_core_vcn.starter_vcn.id}"
-}
-
-resource "oci_core_security_list" "node_sec_list" {
-	compartment_id = var.compartment_ocid
-	display_name = "oke-nodeseclist-quick-starter-virtual-168cd3edc"
-	egress_security_rules {
+ 	egress_security_rules {
 		description = "Allow pods on one worker node to communicate with pods on other worker nodes"
 		destination = local.oke_cidr_nodepool
 		destination_type = "CIDR_BLOCK"
@@ -201,12 +235,17 @@ resource "oci_core_security_list" "node_sec_list" {
 		source = "0.0.0.0/0"
 		stateless = "false"
 	}
-	vcn_id = "${data.oci_core_vcn.starter_vcn.id}"
+
+  freeform_tags = local.freeform_tags
 }
 
-resource "oci_core_security_list" "kubernetes_api_endpoint_sec_list" {
-	compartment_id = var.compartment_ocid
-	display_name = "oke-k8sApiEndpoint-quick-starter-virtual-168cd3edc"
+#----------------------------------------------------------------------------
+
+resource oci_core_security_list starter_seclist_api {
+  compartment_id = local.lz_network_cmp_ocid
+  vcn_id         = data.oci_core_vcn.starter_vcn.id
+  display_name   = "${var.prefix}-seclist-node"
+
 	egress_security_rules {
 		description = "Allow Kubernetes Control Plane to communicate with OKE"
 		destination = "all-fra-services-in-oracle-services-network"
@@ -260,8 +299,84 @@ resource "oci_core_security_list" "kubernetes_api_endpoint_sec_list" {
 		source = local.oke_cidr_nodepool
 		stateless = "false"
 	}
-	vcn_id = "${data.oci_core_vcn.starter_vcn.id}"
+
+  freeform_tags = local.freeform_tags
 }
+
+#----------------------------------------------------------------------------
+# SUBNETS
+
+resource "oci_core_subnet" "starter_nodepool_subnet" {
+  #Required
+  # availability_domain = data.oci_identity_availability_domain.ad1.name
+  cidr_block          = local.oke_cidr_nodepool
+  compartment_id      = local.lz_network_cmp_ocid
+  vcn_id              = data.oci_core_vcn.starter_vcn.id
+  dns_label           = "nodepool"
+  prohibit_public_ip_on_vnic = "true"
+
+  # Provider code tries to maintain compatibility with old versions.
+  # security_list_ids = [oci_core_security_list.starter_seclist_node.id,data.oci_core_vcn.starter_vcn.default_security_list_id,oci_core_security_list.starter_security_list.id]
+  security_list_ids = [oci_core_security_list.starter_seclist_node.id,data.oci_core_vcn.starter_vcn.default_security_list_id]
+  display_name      = "${var.prefix}-oke-nodepool-subnet"
+  route_table_id    = oci_core_route_table.starter_route_private.id
+
+  freeform_tags = local.freeform_tags
+}
+
+resource "oci_core_subnet" "starter_lb_subnet" {
+  #Required
+  cidr_block          = local.oke_cidr_loadbalancer
+  compartment_id      = local.lz_network_cmp_ocid
+  vcn_id              = data.oci_core_vcn.starter_vcn.id
+  dns_label           = "lb"
+  prohibit_public_ip_on_vnic = "false"
+
+  # Provider code tries to maintain compatibility with old versions.
+  # security_list_ids = [data.oci_core_vcn.starter_vcn.default_security_list_id, oci_core_security_list.starter_security_list.id]
+  security_list_ids = [oci_core_security_list.starter_seclist_lb.id,data.oci_core_vcn.starter_vcn.default_security_list_id]
+  display_name      = "${var.prefix}-oke-lb-subnet"
+  route_table_id    = data.oci_core_vcn.starter_vcn.default_route_table_id
+
+  freeform_tags     = local.freeform_tags
+}
+
+resource "oci_core_subnet" "starter_api_subnet" {
+  #Required
+  cidr_block          = local.oke_cidr_api
+  compartment_id      = local.lz_network_cmp_ocid
+  vcn_id              = data.oci_core_vcn.starter_vcn.id
+  dns_label           = "api"
+  prohibit_public_ip_on_vnic = "false"
+
+  # Provider code tries to maintain compatibility with old versions.
+  # security_list_ids = [oci_core_security_list.starter_seclist_api.id,data.oci_core_vcn.starter_vcn.default_security_list_id,oci_core_security_list.starter_security_list.id]
+  security_list_ids = [oci_core_security_list.starter_seclist_api.id,data.oci_core_vcn.starter_vcn.default_security_list_id]
+  display_name      = "${var.prefix}-oke-api-subnet"
+  route_table_id    = data.oci_core_vcn.starter_vcn.default_route_table_id
+  freeform_tags     = local.freeform_tags
+}
+
+# 	security_list_ids = ["${oci_core_security_list.kubernetes_api_endpoint_sec_list.id}"]
+
+/*
+resource "oci_core_subnet" "starter_pod_subnet" {
+  #Required
+  cidr_block          = "10.0.40.0/24"
+  compartment_id      = local.lz_network_cmp_ocid
+  vcn_id              = data.oci_core_vcn.starter_vcn.id
+
+  # Provider code tries to maintain compatibility with old versions.
+  security_list_ids = [data.oci_core_vcn.starter_vcn.default_security_list_id, oci_core_security_list.starter_security_list.id]
+  display_name      = "${var.prefix}-oke-pod-subnet"
+  route_table_id    = data.oci_core_vcn.starter_vcn.default_route_table_id
+  freeform_tags     = local.freeform_tags
+}
+*/
+
+#----------------------------------------------------------------------------
+# CLUSTER
+
 
 resource "oci_containerengine_cluster" "starter_oke" {
 	cluster_pod_network_options {
@@ -270,34 +385,69 @@ resource "oci_containerengine_cluster" "starter_oke" {
 	compartment_id = var.compartment_ocid
 	endpoint_config {
 		is_public_ip_enabled = "true"
-		subnet_id = "${oci_core_subnet.kubernetes_api_endpoint_subnet.id}"
+		subnet_id = "${oci_core_subnet.starter_api_subnet.id}"
 	}
 	freeform_tags = {
-		"OKEclusterName" = "starter-virtual"
+		"OKEclusterName" = "${var.prefix}-oke"
 	}
-	kubernetes_version = "v1.29.1"
-	name = "starter-virtual"
+	kubernetes_version = var.kubernetes_version
+	name = "${var.prefix}-oke"
 	options {
 		admission_controller_options {
 			is_pod_security_policy_enabled = "false"
 		}
 		persistent_volume_config {
 			freeform_tags = {
-				"OKEclusterName" = "starter-virtual"
+				"OKEclusterName" = "${var.prefix}-oke"
 			}
 		}
 		service_lb_config {
 			freeform_tags = {
-				"OKEclusterName" = "starter-virtual"
+				"OKEclusterName" = "${var.prefix}-oke"
 			}
 		}
-		service_lb_subnet_ids = ["${oci_core_subnet.service_lb_subnet.id}"]
+		service_lb_subnet_ids = ["${oci_core_subnet.starter_api_subnet.id}"]
 	}
 	type = "ENHANCED_CLUSTER"
-	vcn_id = "${data.oci_core_vcn.starter_vcn.id}"
+	vcn_id = data.oci_core_vcn.starter_vcn.id
 }
 
-resource "oci_containerengine_virtual_node_pool" "starter_node_pool" {
+/*
+resource "oci_containerengine_cluster" "starter_oke" {
+  #Required
+  compartment_id     = var.compartment_ocid
+  kubernetes_version = var.kubernetes_version
+  name               = "${var.prefix}-oke"
+  vcn_id             = data.oci_core_vcn.starter_vcn.id
+  type               = "ENHANCED_CLUSTER"
+  cluster_pod_network_options {
+    # VNPs require cni_type as OCI_VCN_IP_NATIVE
+    cni_type = "OCI_VCN_IP_NATIVE"
+  }
+  endpoint_config {
+    #Optional
+    is_public_ip_enabled = "true"
+    # nsg_ids              = ["${oci_core_network_security_group.network_security_group_rd.id}"]
+    subnet_id              = oci_core_subnet.starter_api_subnet.id
+  }
+  options {
+    admission_controller_options {
+      is_pod_security_policy_enabled = "false"
+    }
+    service_lb_subnet_ids = oci_core_subnet.starter_api_subnet.id
+  }    
+  freeform_tags = local.freeform_tags
+}
+*/
+
+#----------------------------------------------------------------------------
+# NODE POOL
+
+variable oke_virtual_node_shape {
+  default = "Pod.Standard.E4.Flex"
+}
+
+resource "oci_containerengine_virtual_node_pool" "create_virtual_node_pool_details0" {
 	cluster_id = "${oci_containerengine_cluster.starter_oke.id}"
 	compartment_id = var.compartment_ocid
 	display_name = "pool1"
@@ -307,25 +457,87 @@ resource "oci_containerengine_virtual_node_pool" "starter_node_pool" {
 	}
 	placement_configurations {
 		availability_domain = "KSGd:EU-FRANKFURT-1-AD-1"
-		subnet_id = "${oci_core_subnet.node_subnet.id}"
+		subnet_id = "${oci_core_subnet.starter_nodepool_subnet.id}"
         fault_domain=["FAULT-DOMAIN-1"]
 	}
 	placement_configurations {
 		availability_domain = "KSGd:EU-FRANKFURT-1-AD-2"
-		subnet_id = "${oci_core_subnet.node_subnet.id}"
+		subnet_id = "${oci_core_subnet.starter_nodepool_subnet.id}"
         fault_domain=["FAULT-DOMAIN-1"]
 	}
 	placement_configurations {
 		availability_domain = "KSGd:EU-FRANKFURT-1-AD-3"
-		subnet_id = "${oci_core_subnet.node_subnet.id}"
+		subnet_id = "${oci_core_subnet.starter_nodepool_subnet.id}"
         fault_domain=["FAULT-DOMAIN-1"]
 	}
 	pod_configuration {
-		shape = var.oke_shape
-		subnet_id = "${oci_core_subnet.node_subnet.id}"
+		shape = "Pod.Standard.E4.Flex"
+		subnet_id = "${oci_core_subnet.starter_nodepool_subnet.id}"
 	}
 	size = "1"
 }
+
+#----------------------------------------------------------------------------
+
+
+
+resource "oci_identity_policy" "starter_oke_policy" {
+  provider       = oci.home    
+  name           = "${var.prefix}-oke-policy"
+  description    = "${var.prefix}-oke-policy"
+  compartment_id = var.tenancy_ocid
+  statements = [
+    "define tenancy ske as ocid1.tenancy.oc1..aaaaaaaacrvwsphodcje6wfbc3xsixhzcan5zihki6bvc7xkwqds4tqhzbaq",
+    "define compartment ske_compartment as ocid1.compartment.oc1..aaaaaaaa2bou6r766wmrh5zt3vhu2rwdya7ahn4dfdtwzowb662cmtdc5fea",
+    "endorse any-user to associate compute-container-instances in compartment ske_compartment of tenancy ske with subnets in tenancy where ALL {request.principal.type='virtualnode',request.operation='CreateContainerInstance',request.principal.subnet=2.subnet.id}",
+    "endorse any-user to associate compute-container-instances in compartment ske_compartment of tenancy ske with vnics in tenancy where ALL {request.principal.type='virtualnode',request.operation='CreateContainerInstance',request.principal.subnet=2.subnet.id}",
+    "endorse any-user to associate compute-container-instances in compartment ske_compartment of tenancy ske with network-security-group in tenancy where ALL {request.principal.type='virtualnode',request.operation='CreateContainerInstance'}"
+  ]
+  freeform_tags = local.freeform_tags
+}
+
+#----------------------------------------------------------------------------
+# ADDONS 
+
+/*
+# Database Operator
+resource oci_containerengine_addon starter_oke_addon_dboperator {
+  addon_name                       = "OracleDatabaseOperator"
+  cluster_id                       = oci_containerengine_cluster.starter_oke.id
+  remove_addon_resources_on_delete = "true"
+}
+
+# WebLogic Operator
+resource oci_containerengine_addon starter_oke_addon_wlsoperator {
+  addon_name                       = "WeblogicKubernetesOperator"
+  cluster_id                       = oci_containerengine_cluster.starter_oke.id
+  remove_addon_resources_on_delete = "true"
+}
+
+# CertManager
+resource oci_containerengine_addon starter_oke_addon_certmanager {
+  addon_name                       = "CertManager"
+  cluster_id                       = oci_containerengine_cluster.starter_oke.id
+  remove_addon_resources_on_delete = "true"
+}
+*/
+
+#----------------------------------------------------------------------------
+# OUTPUTS
+
+/*
+output "node_pool" {
+  value = {
+    id                 = oci_containerengine_node_pool.starter_virtual_node_pool.id
+    kubernetes_version = oci_containerengine_node_pool.starter_virtual_node_pool.kubernetes_version
+    name               = oci_containerengine_node_pool.starter_virtual_node_pool.name
+    subnet_ids         = oci_containerengine_node_pool.starter_virtual_node_pool.subnet_ids
+  }
+}
+*/
+
+#----------------------------------------------------------------------------
+# LOCALS
 
 locals {
   oke_ocid = oci_containerengine_cluster.starter_oke.id
@@ -335,3 +547,6 @@ locals {
 output "oke_ocid" {
   value = local.oke_ocid
 }
+
+
+
