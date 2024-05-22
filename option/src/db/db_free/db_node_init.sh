74d638abc0ad2cfc68b9924969fa835a23233b56
Marc Gueury <marc.gueury@oracle.com>
2024-05-22 22:26:23 +0200

    v2.0

--- option/src/db/db_free/db_node_init.sh
+++ option/src/db/db_free/db_node_init.sh
@@ -1,60 +1,43 @@
 # sudo dnf install -y podman
-# podman run -d -p 1521:1522 -p 1522:1522 -p 8443:8443 -p 27017:27017 -e WORKLOAD_TYPE='ATP' -e WALLET_PASSWORD=LiveLab_123 -e ADMIN_PASSWORD=LiveLab_123 --cap-add SYS_ADMIN --device /dev/fuse --name adb-free --volume adb_container_volume:/u01/data container-registry.oracle.com/database/adb-free:latest-23ai
-# alias adb-cli="podman exec adb-free adb-cli"
+# podman run -d -p 1521:1521 -p 1522:1522 -p 8443:8443 -p 27017:27017 -e WORKLOAD_TYPE='ADW' -e WALLET_PASSWORD=${DB_PASSWORD} -e ADMIN_PASSWORD=${DB_PASSWORD} --cap-add SYS_ADMIN --device /dev/fuse --name adb-free --volume adb_container_volume:/u01/data container-registry.oracle.com/database/adb-free:latest-23ai
+alias adb-cli="podman exec adb-free adb-cli"
 
-# Doc: https://docs.oracle.com/en/database/oracle/oracle-database/23/xeinl/installing-oracle-database-free.html
-# Run as root
-FREEDB_RPM=oracle-database-free-23ai-1.0-1.el8.x86_64.rpm
-if [ -f ${FREEDB_RPM} ]; then
-  echo "SKIPPING DB INSTALL - File ${FREEDB_RPM} already downloaded"
-  exit
-fi 
+sudo yum update
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum update
+sudo yum -y install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker opc
 
-dnf install -y oraclelinux-developer-release-el8
-dnf config-manager --set-enabled ol8_developer 
-sudo dnf install -y oracle-database-preinstall-23ai
-
-wget https://download.oracle.com/otn-pub/otn_software/db-free/${FREEDB_RPM} 
-dnf -y localinstall ${FREEDB_RPM} 
-
-# echo DB_PASSWORD=$DB_PASSWORD
-(echo "${DB_PASSWORD}"; echo "${DB_PASSWORD}";) | /etc/init.d/oracle-free-23ai configure
-
-cat >> /home/oracle/.bash_profile << EOF
-
-# Setup Oracle Free environment
-export ORACLE_SID=FREE 
-export ORAENV_ASK=NO 
-. /opt/oracle/product/23ai/dbhomeFree/bin/oraenv
-unset ORAENV_ASK
-EOF
-
-ls -al /usr/local/bin
-if [ "$TF_VAR_language" = "apex" ]; then
-  # Install ORDS in silent mode
-  dnf install -y graalvm22-ee-17-jdk
-  dnf install -y ords
-  chown oracle /etc/ords 
-  cat > /tmp/password.txt << EOF
-${DB_PASSWORD}
-${DB_PASSWORD}
-EOF
-  su - oracle -c "/usr/local/bin/ords --config /etc/ords/config install --admin-user SYS --proxy-user --db-hostname localhost --db-port 1521 --db-servicename FREE --log-folder /etc/ords/logs --feature-sdw true --feature-db-api true --feature-rest-enabled-sql true --password-stdin < /tmp/password.txt"
-  /etc/init.d/ords start
-  firewall-cmd --zone=public --add-port=8080/tcp --permanent
-
-  # Install APEX
-  cd /tmp
-  su - oracle -c "wget https://download.oracle.com/otn_software/apex/apex_23.2_en.zip"
-  su - oracle -c "unzip apex_23.2_en.zip"
-  su - oracle -c "cd apex; sqlplus '/ as sysdba' @apexins.sql SYSAUX SYSAUX TEMP /i/"
-else
-  echo "TF_VAR_language=$TF_VAR_language. APEX not installed."
-fi
+docker run -d -p 1521:1522 -p 1522:1522 -p 8443:8443 -p 27017:27017 -e WORKLOAD_TYPE='ADW' -e WALLET_PASSWORD=${DB_PASSWORD} -e ADMIN_PASSWORD=${DB_PASSWORD} --cap-add SYS_ADMIN --device /dev/fuse --name adb-free --volume adb_container_volume:/u01/data container-registry.oracle.com/database/adb-free:latest-23ai
+alias adb-cli="docker exec adb-free adb-cli"
 
 # Open the Firewall
 firewall-cmd --zone=public --add-port=1521/tcp --permanent
+firewall-cmd --zone=public --add-port=1522/tcp --permanent
+firewall-cmd --zone=public --add-port=8443/tcp --permanent
 firewall-cmd --reload
+
+cat >> /home/opc/.bash_profile << EOF
+alias adb-cli="podman exec adb-free adb-cli"
+EOF
+
+# podman logs adb-free
+# podman exec -it adb-free bash
+# export ORACLE_HOME=/u01/app/oracle/product/23.0.0.0/dbhome_1
+# export PATH=$ORACLE_HOME/bin:$PATH
+# export TNS_ADMIN=/u01/app/oracle/wallets/tls_wallet
+# sqlplus admin/$DB_PASSWORD@myatp_medium
+# myatp_medium = (description=(retry_count=0)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=localhost))(connect_data=(service_name=myatp_medium.adb.oraclecloud.com))(security=(SSL_SERVER_DN_MATCH=TRUE)(SSL_SERVER_CERT_DN="CN=af44605599f8")))
+# myatp_medium1 = (description=(retry_count=0)(retry_delay=3)(address=(protocol=tcp)(port=1521)(host=localhost))(connect_data=(service_name=myatp_medium.adb.oraclecloud.com))(security=(SSL_SERVER_DN_MATCH=TRUE)(SSL_SERVER_CERT_DN="CN=af44605599f8")))
+
+# mkdir scratch
+# podman cp adb-free:/u01/app/oracle/wallets/tls_wallet /scratch/tls_wallet
+# export TNS_ADMIN=/scratch/tls_wallet
+
 
 # -- Root container
 # sqlplus sys/$DB_PASSWORD@//localhost:1521/free as sysdba
