a9ed319ee5e8a3dc084bcaecf96990725b2b8856
Marc Gueury <marc.gueury@oracle.com>
2024-05-23 14:55:22 +0200

    v2.0

--- option/src/db/db_free/db_node_init.sh
+++ option/src/db/db_free/db_node_init.sh
@@ -1,20 +1,21 @@
-# sudo dnf install -y podman
-# podman run -d -p 1521:1521 -p 1522:1522 -p 8443:8443 -p 27017:27017 -e WORKLOAD_TYPE='ADW' -e WALLET_PASSWORD=${DB_PASSWORD} -e ADMIN_PASSWORD=${DB_PASSWORD} --cap-add SYS_ADMIN --device /dev/fuse --name adb-free --volume adb_container_volume:/u01/data container-registry.oracle.com/database/adb-free:latest-23ai
-# alias adb-cli="podman exec adb-free adb-cli"
+setenforce 0
+sed -i 's/enforcing/disabled/g' /etc/sysconfig/selinux
+sudo dnf install -y podman
+podman run -dns 8.8.8.8 -d -p 1521:1521 -p 1522:1522 -p 8443:8443 -p 27017:27017 -e WORKLOAD_TYPE='ATP' -e WALLET_PASSWORD=${DB_PASSWORD} -e ADMIN_PASSWORD=${DB_PASSWORD} --cap-add SYS_ADMIN --device /dev/fuse --name adb-free --volume adb_container_volume:/u01/data container-registry.oracle.com/database/adb-free:latest-23ai
+alias adb-cli="podman exec adb-free adb-cli"
 
 # sudo yum update -y
 # sudo yum update -y
 
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum -y install docker-ce docker-ce-cli containerd.io
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo groupadd docker
-sudo usermod -aG docker opc
-
-docker run -d -p 1521:1522 -p 1522:1522 -p 8443:8443 -p 27017:27017 -e WORKLOAD_TYPE='ADW' -e WALLET_PASSWORD=${DB_PASSWORD} -e ADMIN_PASSWORD=${DB_PASSWORD} --cap-add SYS_ADMIN --device /dev/fuse --name adb-free --volume adb_container_volume:/u01/data container-registry.oracle.com/database/adb-free:latest-23ai
-alias adb-cli="docker exec adb-free adb-cli"
+# sudo yum install -y yum-utils
+# sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+# sudo yum -y install docker-ce docker-ce-cli containerd.io
+# sudo systemctl start docker
+# sudo systemctl enable docker
+# sudo groupadd docker
+# sudo usermod -aG docker opc
+# docker run -d -p 1521:1522 -p 1522:1522 -p 8443:8443 -p 27017:27017 -e WORKLOAD_TYPE='ADW' -e WALLET_PASSWORD=${DB_PASSWORD} -e ADMIN_PASSWORD=${DB_PASSWORD} --cap-add SYS_ADMIN --device /dev/fuse --name adb-free --volume adb_container_volume:/u01/data container-registry.oracle.com/database/adb-free:latest-23ai
+# alias adb-cli="docker exec adb-free adb-cli"
 
 # Open the Firewall
 firewall-cmd --zone=public --add-port=1521/tcp --permanent
@@ -23,7 +24,7 @@ firewall-cmd --zone=public --add-port=8443/tcp --permanent
 firewall-cmd --reload
 
 cat >> /home/opc/.bash_profile << EOF
-alias adb-cli="docker exec adb-free adb-cli"
+alias adb-cli="podman exec adb-free adb-cli"
 EOF
 
 # podman logs adb-free
