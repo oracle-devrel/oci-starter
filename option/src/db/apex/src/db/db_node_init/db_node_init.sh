829b94f83d91c1407bbb34d4851354c63060516f
Marc Gueury <marc.gueury@oracle.com>
2024-08-02 20:44:17 +0200

    v2.0

--- option/src/db/apex/src/db/db_node_init/db_node_init.sh
+++ option/src/db/apex/src/db/db_node_init/db_node_init.sh
@@ -5,5 +5,5 @@ cd $SCRIPT_DIR
 export DB_PASSWORD
 
 # Install APEX and DBMS_CLOUD
-sudo su - root -c "export DB_PASSWORD=$DB_PASSWORD; $SCRIPT_DIR/root_install_apex.sh"
-sudo su - oracle -c "export DB_PASSWORD=$DB_PASSWORD; export DB_URL=$DB_URL; $SCRIPT_DIR/oracle_install_apex.sh"
+sudo su - root -c "export DB_PASSWORD=\"$DB_PASSWORD\"; $SCRIPT_DIR/root_install_apex.sh"
+sudo su - oracle -c "export DB_PASSWORD=\"$DB_PASSWORD\"; export DB_URL=\"$DB_URL\"; $SCRIPT_DIR/oracle_install_apex.sh"
