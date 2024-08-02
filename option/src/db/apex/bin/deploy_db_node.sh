9f1fc4cd0ea8827cdf87c058d06629129005b922
Marc Gueury <marc.gueury@oracle.com>
2024-08-02 20:40:43 +0200

    v2.0

--- option/src/db/apex/bin/deploy_db_node.sh
+++ option/src/db/apex/bin/deploy_db_node.sh
@@ -11,5 +11,5 @@ eval "$(ssh-agent -s)"
 ssh-add $TF_VAR_ssh_private_path
 
 scp -r -o StrictHostKeyChecking=no -oProxyCommand="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -W %h:%p opc@$BASTION_IP" src/db/db_node_init opc@$DB_NODE_IP:/tmp
-ssh -o StrictHostKeyChecking=no -oProxyCommand="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -W %h:%p opc@$BASTION_IP" opc@$DB_NODE_IP "chmod +x /tmp/db_node_init/db_node_init.sh; sudo -i -u root DB_PASSWORD=$TF_VAR_db_password DB_URL=$DB_URL TF_VAR_language=$TF_VAR_language /tmp/db_node_init/db_node_init.sh 2>&1 | tee -a /tmp/db_node_init/db_node_init.log"
+ssh -o StrictHostKeyChecking=no -oProxyCommand="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -W %h:%p opc@$BASTION_IP" opc@$DB_NODE_IP "chmod +x /tmp/db_node_init/db_node_init.sh; sudo -i -u root DB_PASSWORD=\"$TF_VAR_db_password\" DB_URL=\"$DB_URL\" TF_VAR_language=\"$TF_VAR_language\" /tmp/db_node_init/db_node_init.sh 2>&1 | tee -a /tmp/db_node_init/db_node_init.log"
 
