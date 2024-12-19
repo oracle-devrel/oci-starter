38af8665dee49248fe32669c70d278bcb1ca647c
Marc Gueury <marc.gueury@oracle.com>
2024-12-19 14:25:36 +0100

    v3.0

--- basis/build.sh
+++ basis/build.sh
@@ -3,11 +3,11 @@ export PROJECT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null &
 export TARGET_DIR=$PROJECT_DIR/target
 
 cd $PROJECT_DIR
-mkdir -p $TARGET_DIR/log
-export LOG_NAME=$TARGET_DIR/log/build.${DATE_POSTFIX}.log
+mkdir -p $TARGET_DIR/logs
+export LOG_NAME=$TARGET_DIR/logs/build.${DATE_POSTFIX}.log
 
-# Show the log and save it to target/build.log and target/log
-ln -s $LOG_NAME $TARGET_DIR/build.log
+# Show the log and save it to target/build.log and target/logs
+ln -sf $LOG_NAME $TARGET_DIR/build.log
 bin/build_all.sh $@ 2>&1 | tee $LOG_NAME
 # Return the exit code of build_all.sh
 exit ${PIPESTATUS[0]}
--- basis/destroy.sh
+++ basis/destroy.sh
@@ -4,10 +4,10 @@ cd $PROJECT_DIR
 
 export TARGET_DIR=$PROJECT_DIR/target
 DATE_POSTFIX=`date '+%Y%m%d-%H%M%S'`
-LOG_NAME=$TARGET_DIR/log/destroy.${DATE_POSTFIX}.log
+LOG_NAME=$TARGET_DIR/logs/destroy.${DATE_POSTFIX}.log
 
-# Show the log and save it to target/build.log and target/log
-ln -s $LOG_NAME $TARGET_DIR/destroy.log
+# Show the log and save it to target/build.log and target/logs
+ln -sf $LOG_NAME $TARGET_DIR/destroy.log
 bin/destroy_all.sh $@ 2>&1 | tee $LOG_NAME
 # Return the exit code of destroy_all.sh
 exit ${PIPESTATUS[0]}
