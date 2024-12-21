142ecc0d314bfc9b1f856624ec12bb05db41d0b8
Marc Gueury <marc.gueury@oracle.com>
2024-12-21 23:44:47 +0100

    v3.0

--- test_suite/test_errors.sh
+++ test_suite/test_errors.sh
@@ -2,10 +2,5 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-if [ -z "$1" ]; then
-  echo "Usage: test_errors_only.sh <FULL_DIRECTORY_PATH>"
-  exit
-fi
-
-export TEST_ERRORS_ONLY=$1
+export TEST_ERRORS_ONLY=true
 ./test_suite.sh
