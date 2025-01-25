54d768152ed0a7c30cc0797d29b935a3a52252c2
Marc Gueury <marc.gueury@oracle.com>
2025-01-25 17:11:16 +0100

    v3.0

--- basis/starter.sh
+++ basis/starter.sh
@@ -2,7 +2,7 @@
 export PROJECT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ -f $PROJECT_DIR/bin/oci-starter.sh ]; then
-  export PATH=$PATH:$PROJECT_DIR/bin
+  export PATH=$PROJECT_DIR/bin:$PATH
 fi  
 . oci-starter.sh $@
 exit ${PIPESTATUS[0]}
