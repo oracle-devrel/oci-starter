1473f9a651ff9c741c0b79e2835628419165e4d7
Marc Gueury <marc.gueury@oracle.com>
2025-01-25 13:59:40 +0100

    v3.0

--- basis/starter.sh
+++ basis/starter.sh
@@ -2,17 +2,7 @@
 export PROJECT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ -f $PROJECT_DIR/bin/oci-starter.sh ]; then
-  export BIN_DIR=$PROJECT_DIR/bin
-else 
-  BIN_PATH=$(which oci-starter.sh)
-  if [ "$BIN_PATH" == "" ]; then
-    echo "ERROR: oci-starter.sh not found."
-    exit 1
-  else
-    export BIN_DIR=$(dirname "$BIN_PATH")
-    echo "$BIN_DIR"
-  fi 
+  export PATH=$PATH:$PROJECT_DIR/bin
 fi  
-echo "$BIN_DIR"
-. $BIN_DIR/oci-starter.sh $@
+. oci-starter.sh $@
 exit ${PIPESTATUS[0]}
