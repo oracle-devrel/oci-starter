cedebeada8ce5415117713593b1438fcb5318bf4
Marc Gueury <marc.gueury@oracle.com>
2025-01-26 16:16:56 +0100

    v3.0

--- basis/bin/build_common.sh
+++ basis/bin/build_common.sh
@@ -13,7 +13,7 @@ APP_DIR=`echo ${SCRIPT_DIR} |sed -E "s#(.*)/(.*)#\2#"`
 cd $SCRIPT_DIR
 
 if [ -z "$TF_VAR_deploy_type" ]; then
-  . $PROJECT_DIR/env.sh
+  . $PROJECT_DIR/starter.sh env
 else 
   . $BIN_DIR/shared_bash_function.sh
 fi 
