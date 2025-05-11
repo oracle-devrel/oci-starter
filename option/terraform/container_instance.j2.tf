9a345d8b73626310091feb092c9fdeea00fcdb45
Marc Gueury <marc.gueury@oracle.com>
2025-05-11 15:53:53 +0200

    v3.0

--- option/terraform/container_instance.j2.tf
+++ option/terraform/container_instance.j2.tf
@@ -60,7 +60,8 @@ resource oci_container_instances_container_instance starter_container_instance {
 }
 
 # Store the containers in the APP Compartment
-resource "random_string" "repo_prefix" {
+# Avoid container's name conflict by adding a random prefix
+resource "random_string" "container_prefix" {
   length  = 8
   numeric  = false
   special = false
@@ -69,17 +70,17 @@ resource "random_string" "repo_prefix" {
 
 resource "oci_artifacts_container_repository" "starter_repo_app" {
   compartment_id = local.lz_app_cmp_ocid  
-  display_name   = "${random_string.repo_prefix.result}/${var.prefix}-app"
+  display_name   = "${random_string.container_prefix.result}/${var.prefix}-app"
   freeform_tags = local.freeform_tags    
 }
 
 resource "oci_artifacts_container_repository" "starter_repo_ui" {
   compartment_id = local.lz_app_cmp_ocid  
-  display_name   = "${random_string.repo_prefix.result}/${var.prefix}-ui"
+  display_name   = "${random_string.container_prefix.result}/${var.prefix}-ui"
   freeform_tags = local.freeform_tags    
 }
 
-output "repo_prefix" {
-  value = random_string.repo_prefix.result
+output "container_prefix" {
+  value = random_string.container_prefix.result
 }
 
