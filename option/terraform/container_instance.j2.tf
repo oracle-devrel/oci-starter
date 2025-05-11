c08ba588b52dc55ff8b6be08f601dee4fb37bc0d
Marc Gueury <marc.gueury@oracle.com>
2025-05-11 10:52:09 +0200

    v3.0

--- option/terraform/container_instance.j2.tf
+++ option/terraform/container_instance.j2.tf
@@ -58,3 +58,31 @@ resource oci_container_instances_container_instance starter_container_instance {
   }
   freeform_tags = local.freeform_tags    
 }
+
+# Store the containers in the APP Compartment
+resource "random_string" "repo_prefix" {
+  length  = 8
+  numeric  = false
+  special = false
+  upper = false
+}
+
+resource "oci_artifacts_container_repository" "starter_repo_app" {
+  compartment_id = local.lz_app_cmp_ocid  
+  display_name   = "${random_string.repo_prefix.result}/${var.prefix}-app"
+  freeform_tags = local.freeform_tags    
+}
+
+resource "oci_artifacts_container_repository" "starter_repo_ui" {
+  compartment_id = local.lz_app_cmp_ocid  
+  display_name   = "${random_string.repo_prefix.result}/${var.prefix}-ui"
+  freeform_tags = local.freeform_tags    
+}
+
+output "repo_app" {
+  value = oci_artifacts_container_repository.starter_repo_app.id
+}
+
+output "repo_ui" {
+  value = oci_artifacts_container_repository.starter_repo_ui.id
+}
