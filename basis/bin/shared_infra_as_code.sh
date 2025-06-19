0615957ab4c77f75dc4789e78d701dfc34f57408
Marc Gueury <marc.gueury@oracle.com>
2025-06-19 19:17:43 +0200

    v3.0

--- basis/bin/shared_infra_as_code.sh
+++ basis/bin/shared_infra_as_code.sh
@@ -26,9 +26,11 @@ infra_as_code_plan() {
 # Before to run the build check the some resource with unique name in the tenancy does not exists already
 infra_as_code_precheck() {
   title "Precheck"
+  cd $PROJECT_DIR/src/terraform 
+  $TERRAFORM_COMMAND init -no-color -upgrade  
   $TERRAFORM_COMMAND plan -json -out=$TARGET_DIR/tfplan.out
   # Buckets
-  LIST_BUCKETS=`$TERRAFORM_COMMAND show -json $TARGET_DIR/tfplan.out | jq '.resource_changes[] | select(.change.actions[] == "create") | .address' | grep "oci_objectstorage_bucket"` 
+  LIST_BUCKETS=`$TERRAFORM_COMMAND show -json $TARGET_DIR/tfplan.out 2>/dev/null | jq '.resource_changes[] | select(.change.actions[] == "create") | .address' | grep "oci_objectstorage_bucket"` 
   for BUCKET_NAME in `LIST_BUCKETS`; do
     BUCKET_CHECK=`oci os bucket get --bucket-name $BUCKET_NAME --namespace-name $TF_VAR_namespace | jq -r .data.name`
     if [ "$BUCKET_NAME" == "$BUCKET_CHECK" ]; then
