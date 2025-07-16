79a16c621758c59032011fb6360888dd73cbf9a2
Marc Gueury <marc.gueury@oracle.com>
2025-07-16 16:18:44 +0200

    v3.0

--- basis/src/terraform/build.j21.tf
+++ basis/src/terraform/build.j21.tf
@@ -1,14 +1,6 @@
 ## BUILD_DEPLOY
-resource "null_resource" "build_deploy" {
-  provisioner "local-exec" {
-    command = <<-EOT
-        pwd
-        cat target/terraform.tfstate
-        export
-        ./starter.sh frm build_deploy
-        cat target/resource_manager_variables.json
-        EOT
-  }
+data "external" "env" {
+  program = ["bash", "-c", "./starter.sh frm build_deploy 1>&2; cat target/resource_manager_variables.json"]
   depends_on = [
 {%- for key in terraform_resources %}
     {{key}},
@@ -16,19 +8,16 @@ resource "null_resource" "build_deploy" {
   ]
 }
 
-{%- if terraform_resources_part2|length>0 %}
-# PART2
-#
-# In case like instance_pool, oke, function, container_instance, ...
-# More terraform resources need to be created after build_deploy.
-# Reread the env viables
-data "external" "env_part2" {
-  program = ["cat", "target/resource_manager_variables.json"]
+resource "null_resource" "log_frm_build_deploy" {
+  provisioner "local-exec" {
+    command = <<-EOT
+        cat target/frm_build_deploy.log
+        EOT
+  }
   depends_on = [
-    null_resource.build_deploy
-  ]
+    data.external.env
+  ]   
 }
-{%- endif %}
 
 ## AFTER_BUILD
 # Last action at the end of the build
@@ -40,7 +29,7 @@ resource "null_resource" "after_build" {
 {%- for key in terraform_resources_part2 %}
     {{key}},
 {%- endfor %}   
-    null_resource.build_deploy   
+    null_resource.log_frm_build_deploy   
   ]
 
   provisioner "local-exec" {
