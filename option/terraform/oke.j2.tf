e0b40faa212b32fd65cb5d778d5f9af76547a416
Marc Gueury <marc.gueury@oracle.com>
2025-07-17 16:24:19 +0200

    v3.0

--- option/terraform/oke.j2.tf
+++ option/terraform/oke.j2.tf
@@ -453,7 +453,7 @@ resource "oci_containerengine_node_pool" "starter_node_pool" {
 
 /*
 # Database Operator
-resource oci_containerengine_addon starter_oke_addon_dboperator {
+# resource oci_containerengine_addon starter_oke_addon_dboperator {
   addon_name                       = "OracleDatabaseOperator"
   cluster_id                       = oci_containerengine_cluster.starter_oke.id
   remove_addon_resources_on_delete = "true"
