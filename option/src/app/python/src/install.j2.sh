5f079a07d1029f6f615900af145a6a3805a72700
Marc Gueury <marc.gueury@oracle.com>
2025-09-16 09:57:53 +0200

    v4.0

--- option/src/app/python/src/install.j2.sh
+++ option/src/app/python/src/install.j2.sh
@@ -9,14 +9,11 @@ sudo yum -y install postgresql-devel
 {%- endif %}
 
 sudo dnf install -y python3.12 python3.12-pip python3-devel
+# sudo pip3.12 install pip --upgrade
 sudo update-alternatives --set python /usr/bin/python3.12
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
-sudo pip3.12 install pip --upgrade
-
 # Install virtual env python_env
-python -m venv myenv
-source myenv/bin/activate
-pip3 install --upgrade pip
-# pip3 install -r requirements.txt
-uv pip install --system -r src/requirements.txt
+uv venv myenv/
+source myvenv/bin/activate
+uv pip install -r requirements.txt
--- option/src/app/python/src/start.j2.sh
+++ option/src/app/python/src/start.j2.sh
@@ -5,4 +5,4 @@ cd $SCRIPT_DIR
 {% import "start_sh.j2_macro" as m with context %}
 {{ m.env() }}
 source myenv/bin/activate
-python3 app.py 2>&1 | tee app.log
+python app.py 2>&1 | tee app.log
