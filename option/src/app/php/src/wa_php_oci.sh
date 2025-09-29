34662135026e268d4d602f0a1b67d341b7f860ef
Marc Gueury <marc.gueury@oracle.com>
2025-09-29 20:44:10 +0200

    v4.0

--- option/src/app/php/src/wa_php_oci.sh
+++ option/src/app/php/src/wa_php_oci.sh
@@ -3,14 +3,14 @@
 mkdir -p /run/php-fpm
 
 dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-dnf module enable php:remi-8.5 -y
+dnf module enable php:remi-8.4 -y
 dnf module reset php -y
-dnf module enable php:remi-8.5 -y
+dnf module enable php:remi-8.4 -y
 dnf update php\* -y
 dnf install -y php php-cli php-common php-fpm php-pear gcc curl-devel php-devel zlib-devel pcre-devel php-pecl-http php-mysqlnd systemtap-sdt-devel
-dnf install -y php85-php-oci8
+dnf install -y php84-php-oci8
 # Not sure why I have to copy it
-cp /opt/remi/php85/root/usr/lib64/php/modules/oci8.so /usr/lib64/php/modules/.
+cp /opt/remi/php84/root/usr/lib64/php/modules/oci8.so /usr/lib64/php/modules/.
 php -v
 # export PHP_DTRACE=yes
 # pecl install oci8
