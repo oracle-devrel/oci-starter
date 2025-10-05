66145441a55ab5f85b47f4e4d7aef58c2a3dca59
Marc Gueury <marc.gueury@oracle.com>
2025-10-05 21:38:51 +0200

    v4.0

--- option/terraform/oke_virtual_node.j2.tf
+++ option/terraform/oke_virtual_node.j2.tf
@@ -51,11 +51,6 @@ data "oci_identity_availability_domain" "ad1" {
   ad_number      = 1
 }
 
-data "oci_identity_availability_domain" "ad2" {
-  compartment_id = var.tenancy_ocid
-  ad_number      = 2
-}
-
 data "oci_core_images" "shape_specific_images" {
   #Required
   compartment_id = var.tenancy_ocid
