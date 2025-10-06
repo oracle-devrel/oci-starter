8cd5ed80d35c1e433ce352b13605679f33950632
Marc Gueury <marc.gueury@oracle.com>
2025-10-06 20:21:35 +0200

    v4.0

--- basis/bin/gen_auth_token.sh
+++ basis/bin/gen_auth_token.sh
@@ -8,21 +8,22 @@ cd $PROJECT_DIR
 # Shared BASH Functions
 . $BIN_DIR/shared_bash_function.sh
 
-if grep -q 'TF_VAR_auth_token="__TO_FILL__"' $PROJECT_DIR/env.sh; then
+if grep -q 'auth_token="__TO_FILL__"' $PROJECT_DIR/terraform.tfvars; then
   echo "Generating a new AUTH_TOKEN (Home Region=$TF_VAR_home_region)"
   get_user_details
   oci iam auth-token create --description "OCI_STARTER_TOKEN" --user-id $TF_VAR_current_user_ocid --region $TF_VAR_home_region > auth_token.log 2>&1
+  exit_error "gen_auth_token - Go to  OCI Console/Profile/<your name>/Tokens and Key. Try to generate an Auth Token and place it in terraform.tfvars"
   export TF_VAR_auth_token=`cat auth_token.log | jq -r '.data.token'`
 
   cat auth_token.log
   rm auth_token.log
 
   if [ "$TF_VAR_auth_token" != "" ]; then
-    sed -i "s&TF_VAR_auth_token=\"__TO_FILL__\"&TF_VAR_auth_token=\"$TF_VAR_auth_token\"&" $PROJECT_DIR/env.sh
-    echo "AUTH_TOKEN stored in env.sh"
-    echo "> TF_VAR_auth_token=$TF_VAR_auth_token"
+    sed -i "s&auth_token=\"__TO_FILL__\"&auth_token=\"$TF_VAR_auth_token\"&" $PROJECT_DIR/terraform.tfvars
+    echo "AUTH_TOKEN stored in terraform.tfvars"
+    echo "> auth_token=$TF_VAR_auth_token"
   fi  
 else
-  echo 'File env.sh does not contain: TF_VAR_auth_token="__TO_FILL__"'  
+  echo 'File terraform.tfvars does not contain: auth_token="__TO_FILL__"'  
 fi
 
