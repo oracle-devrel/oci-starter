189aad96747e3bb53254bc74d253371dfa24eda0
Marc Gueury <marc.gueury@oracle.com>
2026-04-24 18:53:42 +0200

    v5.0

--- test_suite/test_bastion_lock.sh
+++ test_suite/test_bastion_lock.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 LOCKFILE="$HOME/bastion_lock"
-TIMEOUT=100
+TIMEOUT=300
 WAIT=5
 ELAPSED=0
+DATE_POSTFIX=`date '+%Y%m%d-%H%M%S'`
+NAME=$DATE_POSTFIX - $1"
+echo "$NAME" >> bastion_lock_waiting
 
 while [ "$ELAPSED" -lt "$TIMEOUT" ]; do
     if [ -e "$LOCKFILE" ]; then
@@ -12,7 +15,7 @@ while [ "$ELAPSED" -lt "$TIMEOUT" ]; do
         # Try to create the lock atomically
         if ( set -o noclobber; > "$LOCKFILE" ) 2> /dev/null; then
             echo "Lock acquired."
-            echo "$1" >> bastion_lock_history
+            sed -i "s&$NAME&$NAME - $ELAPSED secs" bastion_lock_waiting     
             rm -Rf $HOME/app/*
             exit 0
         else
@@ -24,5 +27,6 @@ while [ "$ELAPSED" -lt "$TIMEOUT" ]; do
     ELAPSED=$((ELAPSED + WAIT))
 done
 
+sed -i "s&$NAME&$NAME - ERROR TIMEOUT" bastion_lock_waiting     
 echo "Failed to acquire lock after ${TIMEOUT} seconds."
 exit 1
