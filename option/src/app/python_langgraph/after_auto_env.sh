025a2ff51764e25f31376869798997654cf74891
Marc Gueury <marc.gueury@oracle.com>
2026-04-25 01:18:16 +0200

    v5.0

--- option/src/app/python_langgraph/after_auto_env.sh
+++
@@ -1,9 +0,0 @@
-XXXXX
-# Kubernetes
-if [ "$TF_VAR_deploy_type" == "kubernetes" ]; then
-    append_tf_env "export LANGGRAPH_URL=\"http://langgraph-service:2024\""
-    append_tf_env "export MCP_SERVER_URL=\"http://mcp-server-service:2025/mcp\""
-else 
-    append_tf_env "export LANGGRAPH_URL=\"http://127.0.0.1:2024\""
-    append_tf_env "export MCP_SERVER_URL=\"http://localhost:2025/mcp\""
-fi
