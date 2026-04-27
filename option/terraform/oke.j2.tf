81721905cb8beb1b10756f469743ecea7f6c49db
Marc Gueury <marc.gueury@oracle.com>
2026-04-27 07:24:44 +0200

    v5.0

--- option/terraform/oke.j2.tf
+++ option/terraform/oke.j2.tf
@@ -520,7 +520,7 @@ output "oke_ocid" {
 # Doc: https://docs.oracle.com/en-us/iaas/Content/ContEng/Tasks/contengsettingupnativeingresscontroller-addon-prereqs.htm#contengsettingupnativeingresscontroller-addon-permissions
 resource "oci_identity_policy" "starter_oke_policy" {
     provider       = oci.home    
-    name           = "${var.prefix}-oke-policy"
+    name           = "${var.prefix}-oke-policy-${random_string.id.result}"
     description    = "${var.prefix}-oke-policy"
     compartment_id = var.tenancy_ocid
     statements = [
