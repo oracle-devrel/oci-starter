16b173e53b92f1ee72408e612925dde47b9ad608
Marc Gueury <marc.gueury@oracle.com>
2026-04-30 00:53:01 +0200

    v5.0

--- basis/bin/destroy_oke.sh
+++ basis/bin/destroy_oke.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-. $SCRIPT_DIR/../starter.sh env -no-auto -silent
-. $BIN_DIR/build_common.sh
+. $SCRIPT_DIR/../starter.sh env -silent
 cd $PROJECT_DIR
 
 title "OKE Destroy"
