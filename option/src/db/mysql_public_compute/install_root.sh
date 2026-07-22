56a4d93408872fa0a5b11c316c806365d8575940
Marc Gueury <marcgueury@Marcs-MacBook-Pro.local>
2026-07-22 08:23:01 +0200

    v5.0

--- option/src/db/mysql_public_compute/install_root.sh
+++ option/src/db/mysql_public_compute/install_root.sh
@@ -21,7 +21,7 @@ gpgcheck=1
 gpgkey=https://yum.oracle.com/RPM-GPG-KEY-oracle-ol10
 EOF
 
-tee /etc/yum.repos.d/mysql97-community.repo > /dev/null <<'EOF'
+tee /etc/yum.repos.d/mysql97-tools-community.repo > /dev/null <<'EOF'
 [ol10_mysql97_tools_community]
 name=Oracle Linux 10 MySQL 9.7 Tools Community
 baseurl=https://yum.oracle.com/repo/OracleLinux/OL10/MySQL97/tools/community/x86_64/
