212223c6b79332c264439ea251d5cc85d9364e06
Marc Gueury <marcgueury@Marcs-MacBook-Pro.local>
2026-07-23 17:08:46 +0200

    v5.0

--- option/src/app/apex/apex/install.sh
+++ option/src/app/apex/apex/install.sh
@@ -1,4 +1,4 @@
-sudo dnf module reset nginx -y
-sudo dnf module enable nginx:1.22 -y
-# sudo yum remove nginx-mod*
-# sudo yum install nginx-mod-*
+# Nothing to do on OL10
+
+# sudo dnf module reset nginx -y
+# sudo dnf module enable nginx:1.22 -y
