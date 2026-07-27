5410897c258c8e3d0ed0d677da008cc3f1456780
Marc Gueury <marcgueury@Marcs-MacBook-Pro.local>
2026-07-27 19:05:49 +0200

    v5.0

--- option/src/app/python_langgraph/rest/start.j2.sh
+++ option/src/app/python_langgraph/rest/start.j2.sh
@@ -3,14 +3,17 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 export PATH=~/.local/bin/:$PATH
 
-. $HOME/compute/tf_env.sh
+. $HOME/compute/shared_compute.sh
 
-{%- if deploy_type == "public_compute" %}
+{%- if deploy_type != "public_compute" %}
+export MCP_SERVER_URL="https://$APIGW_HOSTNAME/$TF_VAR_prefix/mcp_server/mcp"
+{%- elif python_framework == "responses" %}
+# Responses API can also access internet MCP servers
 export MCP_SERVER_URL="http://$BASTION_IP/mcp_server/mcp"
 {%- else %}
-export MCP_SERVER_URL="https://$APIGW_HOSTNAME/$TF_VAR_prefix/mcp_server/mcp"
+export MCP_SERVER_URL="https://127.0.0.1:2025/mcp"
 {%- endif %}
 
 source myenv/bin/activate
-port_wait 8080 | tee rest.log
+# port_wait 8080 | tee rest.log
 python rest.py 2>&1 | tee rest.log
